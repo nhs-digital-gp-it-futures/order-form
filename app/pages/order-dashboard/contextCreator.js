27b383a4082c004395431d062e9556753d496665
Fawad Ali <alibombaye@gmail.com>
2020-05-07 10:25:47 +0100

    fixed backLink

--- app/pages/order-dashboard/contextCreator.js
+++ app/pages/order-dashboard/contextCreator.js
@@ -1,6 +1,8 @@
 import neworderPageManifest from './neworder/manifest.json';
+import { baseUrl } from '../../config';
 
 export const getContext = ({ pageName }) => ({
   ...neworderPageManifest,
+  backLinkHref: `${baseUrl}/organisation`,
   pageName,
 });
--- app/pages/order-dashboard/contextCreator.test.js
+++ app/pages/order-dashboard/contextCreator.test.js
@@ -1,10 +1,12 @@
 import newOrderManifest from './neworder/manifest.json';
 import { getContext } from './contextCreator';
+import { baseUrl } from '../../config';
 
 describe('order-dashboard contextCreator', () => {
   describe('getContext', () => {
     it('should return the contents of the new order manifest', () => {
       const context = getContext({});
+      expect(context.backLinkText).toEqual(newOrderManifest.backLinkText);
       expect(context.title).toEqual(newOrderManifest.title);
       expect(context.description).toEqual(newOrderManifest.description);
     });
@@ -12,6 +14,11 @@ describe('order-dashboard contextCreator', () => {
     it('should return the pageName provided', () => {
       const context = getContext({ pageName: 'some-page-name' });
       expect(context.pageName).toEqual('some-page-name');
+    });
+
+    it('should return the backLinkHref', () => {
+      const context = getContext({ pageName: 'some-page-name' });
+      expect(context.backLinkHref).toEqual(`${baseUrl}/organisation`);
     });
   });
 });
