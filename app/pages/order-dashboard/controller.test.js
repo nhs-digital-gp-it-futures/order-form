99287d015ff43f9b8f602ab72f2ca677afcf0843
Fawad Ali <alibombaye@gmail.com>
2020-05-07 08:40:19 +0100

    add controller test

---
+++ app/pages/order-dashboard/controller.test.js
@@ -0,0 +1,20 @@
+import { getNewOrderPageContext } from './controller';
+import * as contextCreator from './contextCreator';
+
+jest.mock('./contextCreator', () => ({
+  getContext: jest.fn(),
+}));
+
+describe('order-dashboard controller', () => {
+  describe('getNewOrderPageContext', () => {
+    it('should call getContext with the correct params when user data is returned by the apiProvider', async () => {
+      contextCreator.getContext
+        .mockResolvedValueOnce();
+
+      await getNewOrderPageContext();
+
+      expect(contextCreator.getContext.mock.calls.length).toEqual(1);
+      expect(contextCreator.getContext).toHaveBeenCalledWith({ pageName: 'neworder' });
+    });
+  });
+});
