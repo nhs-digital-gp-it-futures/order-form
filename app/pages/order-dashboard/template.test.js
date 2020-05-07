ed2b6a6b57a2b57f86c4c2d70b61d9b6fb893fa5
Fawad Ali <alibombaye@gmail.com>
2020-05-07 10:35:35 +0100

    added ui tests

--- app/pages/order-dashboard/template.test.js
+++ app/pages/order-dashboard/template.test.js
@@ -32,7 +32,7 @@ describe('neworder dashboard page', () => {
 
   it('should render the neworder dashboard title', componentTester(setup, (harness) => {
     harness.request(neworderPageContext, ($) => {
-      const neworderPageTitle = $('[data-test-id="neworder-title"]');
+      const neworderPageTitle = $('[data-test-id="neworder-page-title"]');
       expect(neworderPageTitle.length).toEqual(1);
       expect(neworderPageTitle.text().trim()).toEqual(neworderPageContext.title);
     });
@@ -40,7 +40,7 @@ describe('neworder dashboard page', () => {
 
   it('should render the neworder dashboard description', componentTester(setup, (harness) => {
     harness.request(neworderPageContext, ($) => {
-      const neworderPageDescription = $('[data-test-id="neworder-description"]');
+      const neworderPageDescription = $('[data-test-id="neworder-page-description"]');
       expect(neworderPageDescription.length).toEqual(1);
       expect(neworderPageDescription.text().trim()).toEqual(neworderPageContext.description);
     });
