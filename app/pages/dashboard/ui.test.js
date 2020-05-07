af8664c588d93096da5f3c0c889d37432fc02145
kat1906 <katie.armstrong@bjss.com>
2020-05-07 09:34:48 +0100

    fix test

--- app/pages/dashboard/ui.test.js
+++ app/pages/dashboard/ui.test.js
@@ -97,7 +97,7 @@ test('should render add new order button', async (t) => {
   await t
     .expect(button.exists).ok()
     .expect(await extractInnerText(button)).eql(content.newOrderButtonText)
-    .expect(button.getAttribute('href')).eql(`${baseUrl}/order/organisation/neworder`${baseUrl});
+    .expect(button.getAttribute('href')).eql(`${baseUrl}/order/organisation/neworder`);
 });
 
 test('should render the proxy link', async (t) => {
