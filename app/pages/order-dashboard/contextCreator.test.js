a46d244ab138e695a6b28c5f7c8d6e4929855775
Fawad Ali <alibombaye@gmail.com>
2020-05-07 08:53:15 +0100

    added contextCreator test

---
+++ app/pages/order-dashboard/contextCreator.test.js
@@ -0,0 +1,17 @@
+import newOrderManifest from  './neworder/manifest.json';
+import { getContext } from './contextCreator';
+
+describe('order-dashboard contextCreator', () => {
+  describe('getContext', () => {
+    it('should return the contents of the new order manifest', () => {
+      const context = getContext({});
+      expect(context.title).toEqual(newOrderManifest.title);
+      expect(context.description).toEqual(newOrderManifest.description);
+    });
+
+    it('should return the pageName provided', () => {
+      const context = getContext({ pageName: 'some-page-name' });
+      expect(context.pageName).toEqual('some-page-name');
+    });
+  });
+});
