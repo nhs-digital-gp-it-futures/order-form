27a4b18fbb412eeb64f7c27039ad6380c3502c1e
Fawad Ali <alibombaye@gmail.com>
2020-05-07 11:41:02 +0100

    rename order-dashboard to order-task-list

--- app/pages/order-task-list/template.test.js
+++ app/pages/order-task-list/template.test.js
@@ -3,11 +3,11 @@ import manifest from './neworder/manifest.json';
 
 const setup = {
   template: {
-    path: 'pages/order-dashboard/template.njk',
+    path: 'pages/order-task-list/template.njk',
   },
 };
 
-describe('neworder dashboard page', () => {
+describe('neworder task-list page', () => {
   const neworderPageContext = {
     ...manifest,
     pageName: 'neworder',
@@ -23,14 +23,14 @@ describe('neworder dashboard page', () => {
     });
   }));
 
-  it('should render the neworder dashboard page', componentTester(setup, (harness) => {
+  it('should render the neworder task-list page', componentTester(setup, (harness) => {
     harness.request(neworderPageContext, ($) => {
       const neworderPage = $('[data-test-id="neworder-page"]');
       expect(neworderPage.length).toEqual(1);
     });
   }));
 
-  it('should render the neworder dashboard title', componentTester(setup, (harness) => {
+  it('should render the neworder task-list title', componentTester(setup, (harness) => {
     harness.request(neworderPageContext, ($) => {
       const neworderPageTitle = $('[data-test-id="neworder-page-title"]');
       expect(neworderPageTitle.length).toEqual(1);
@@ -38,7 +38,7 @@ describe('neworder dashboard page', () => {
     });
   }));
 
-  it('should render the neworder dashboard description', componentTester(setup, (harness) => {
+  it('should render the neworder task-list description', componentTester(setup, (harness) => {
     harness.request(neworderPageContext, ($) => {
       const neworderPageDescription = $('[data-test-id="neworder-page-description"]');
       expect(neworderPageDescription.length).toEqual(1);
--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ import { logger } from './logger';
 import { withCatch, getHealthCheckDependencies } from './helpers/routerHelper';
 import { getDashboardContext } from './pages/dashboard/controller';
 import includesContext from './includes/manifest.json';
-import { getNewOrderPageContext } from './pages/order-dashboard/controller';
+import { getNewOrderPageContext } from './pages/order-task-list/controller';
 
 const addContext = ({ context, user, csrfToken }) => ({
   ...context,
@@ -38,7 +38,7 @@ export const routes = (authProvider) => {
 
   router.get('/organisation/neworder', authProvider.authorise({ claim: 'ordering' }), withCatch(authProvider, async (req, res) => {
     const context = getNewOrderPageContext();
-    res.render('pages/order-dashboard/template.njk', addContext({ context, user: req.user }));
+    res.render('pages/order-task-list/template.njk', addContext({ context, user: req.user }));
   }));
 
   router.get('/organisation/neworder/description', authProvider.authorise({ claim: 'ordering' }), withCatch(authProvider, async (req, res) => {
