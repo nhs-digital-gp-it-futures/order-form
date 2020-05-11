e8c4856a4ae07dc8d27d7e1ee2f6c3922b3db75b
kat1906 <katie.armstrong@bjss.com>
2020-05-11 16:17:02 +0100

    add function to post the description

--- app/pages/items/description/controller.js
+++ app/pages/items/description/controller.js
@@ -1,3 +1,34 @@
+import { postData } from 'buying-catalogue-library';
 import { getContext } from './contextCreator';
+import { getEndpoint } from '../../../endpoints';
+import { logger } from '../../../logger';
 
 export const getDescriptionContext = params => getContext(params);
+
+export const postOrPatchDescription = async ({ orderId, accessToken, data }) => {
+  const isNewOrder = orderId === 'neworder';
+  // TODO: add PATCH endpoint
+  const endpoint = isNewOrder ? getEndpoint({ endpointLocator: 'postDescription' }) : null;
+  const body = { orderDescription: data.description };
+  const apiCallParams = {
+    endpoint,
+    body,
+    accessToken,
+    logger,
+  };
+
+  try {
+    // TODO: call patchData if not new order
+    const response = isNewOrder ? await postData(apiCallParams) : {};
+    // const response = { data: { orderId: 'order-id-1' }}
+    logger.info(`Order ${isNewOrder ? 'added' : 'updated'} - id: ${response.data.id}, ${JSON.stringify(body)}`);
+    return { success: true, orderId: isNewOrder ? response.data.orderId : orderId };
+  } catch (err) {
+    if (err.response.status === 400 && err.response.data && err.response.data.errors) {
+      // TODO: validation
+      return { success: false };
+    }
+    logger.error('Error adding order');
+    throw new Error();
+  }
+};
---
+++ app/pages/items/description/controller.test.js
@@ -0,0 +1,63 @@
+import { postData } from 'buying-catalogue-library';
+import { postOrPatchDescription } from './controller';
+import * as contextCreator from './contextCreator';
+import { logger } from '../../../logger';
+import { orderApiUrl } from '../../../config';
+
+jest.mock('buying-catalogue-library');
+
+jest.mock('./contextCreator', () => ({
+  getContext: jest.fn(),
+}));
+
+describe('description controller', () => {
+  describe('postOrPatchDescription', () => {
+    afterEach(() => {
+      postData.mockReset();
+    });
+
+    it('should call postData once with the correct params for neworder', async () => {
+      postData
+        .mockResolvedValueOnce({ data: { orderId: 'order1' } });
+
+      await postOrPatchDescription({ orderId: 'neworder', data: { description: 'an order description' }, accessToken: 'access_token' });
+      expect(postData.mock.calls.length).toEqual(1);
+      expect(postData).toHaveBeenCalledWith({
+        endpoint: `${orderApiUrl}/api/v1/order`,
+        body: { orderDescription: 'an order description' },
+        accessToken: 'access_token',
+        logger,
+      });
+    });
+
+    it('should return true if api request is successful', async () => {
+      postData
+        .mockResolvedValueOnce({ data: { orderId: 'order1' } });
+
+      const response = await postOrPatchDescription({ orderId: 'neworder', data: { description: 'an order description' }, accessToken: 'access_token' });
+
+      expect(response.success).toEqual(true);
+    });
+
+    // TODO: validation
+    // it('should return error.respose.data if api request is unsuccessful with 400', async () => {
+    //   postData
+    //     .mockRejectedValueOnce({ response: { status: 400, data: '400 response data' } });
+
+    // const response = await postOrPatchDescription({ orderId: 'neworder', data: { description: 'an order description' }, accessToken: 'access_token' });
+
+    //   expect(response).toEqual('400 response data');
+    // });
+
+    // it('should throw an error if api request is unsuccessful with non 400', async () => {
+    //   postData
+    //     .mockRejectedValueOnce({ response: { status: 500, data: '500 response data' } });
+
+    //   try {
+    //     await postOrPatchDescription({ orderId: 'neworder', data: { description: 'an order description' }, accessToken: 'access_t
+    //   } catch (err) {
+    //     expect(err).toEqual(new Error());
+    //   }
+    // });
+  });
+});
