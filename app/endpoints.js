8eb4b53fbf3e12b5345c746347b990209003d504
kat1906 <katie.armstrong@bjss.com>
2020-05-11 15:50:31 +0100

    add postDescription endpoint

--- app/endpoints.js
+++ app/endpoints.js
@@ -3,6 +3,7 @@ import { oidcBaseUri, orderApiUrl } from './config';
 const endpoints = {
   getIdentityApiHealth: () => `${oidcBaseUri}/health/ready`,
   getOrders: () => `${orderApiUrl}/api/v1/orders`,
+  postDescription: () => `${orderApiUrl}/api/vi/order`,
 };
 
 export const getEndpoint = ({ endpointLocator, options }) => endpoints[endpointLocator](options);
