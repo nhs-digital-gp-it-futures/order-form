1f6a95f2ef11547373c0a5f85e016a6df15292d4
kat1906 <katie.armstrong@bjss.com>
2020-05-11 09:16:58 +0100

    add handling for incorrect date string or missing string

--- app/helpers/dateFormatter.js
+++ app/helpers/dateFormatter.js
@@ -1,5 +1,6 @@
 export const formatDate = (date) => {
   const d = new Date(date);
+  if (Number.isNaN(d.getTime())) return '';
   const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
   const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
   const da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);
