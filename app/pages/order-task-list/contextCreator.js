e0e395c48976198dbc4c29e1b03a826285bec8e7
Fawad Ali <alibombaye@gmail.com>
2020-05-11 14:38:26 +0100

    WIP adding taskList to order-task-list page

--- app/pages/order-task-list/contextCreator.js
+++ app/pages/order-task-list/contextCreator.js
@@ -1,8 +1,10 @@
 import neworderPageManifest from './neworder/manifest.json';
+import taskListManifest from './taskListManifest.json';
 import { baseUrl } from '../../config';
 
 export const getContext = ({ pageName }) => ({
   ...neworderPageManifest,
+  ...taskListManifest,
   backLinkHref: `${baseUrl}/organisation`,
   pageName,
 });
