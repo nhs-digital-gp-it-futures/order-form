0334770f390b447fdee229b7d809223d8ce31d63
kat1906 <katie.armstrong@bjss.com>
2020-05-12 13:37:55 +0100

    amed endpoint

--- app/endpoints.js
+++ app/endpoints.js
@@ -3,7 +3,7 @@ import { oidcBaseUri, orderApiUrl } from './config';
 const endpoints = {
   getIdentityApiHealth: () => `${oidcBaseUri}/health/ready`,
   getOrders: () => `${orderApiUrl}/api/v1/orders`,
-  postDescription: () => `${orderApiUrl}/api/v1/order`,
+  postDescription: () => `${orderApiUrl}/api/v1/orders`,
 };
 
 export const getEndpoint = ({ endpointLocator, options }) => endpoints[endpointLocator](options);
