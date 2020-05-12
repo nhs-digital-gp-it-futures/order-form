232c0d9b135ae14c4dfd2c3c89ebda0249035e3a
kat1906 <katie.armstrong@bjss.com>
2020-05-12 17:33:44 +0100

    add putDescription endpoint

--- app/endpoints.js
+++ app/endpoints.js
@@ -4,6 +4,7 @@ const endpoints = {
   getIdentityApiHealth: () => `${oidcBaseUri}/health/ready`,
   getOrders: options => `${orderApiUrl}/api/v1/organisation/${options.orgId}/orders`,
   postDescription: () => `${orderApiUrl}/api/v1/orders`,
+  putDescription: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/description`,
 };
 
 export const getEndpoint = ({ endpointLocator, options }) => endpoints[endpointLocator](options);
