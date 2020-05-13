f2138657a7b0b532da9b62a962b45870c4987339
Ben.Hayward <ben.hayward@bjss.com>
2020-05-13 14:29:04 +0100

    5161: Update endpoint function name

--- app/pages/order-task-list/controller.js
+++ app/pages/order-task-list/controller.js
@@ -6,7 +6,7 @@ import { logger } from '../../logger';
 const getNewOrderTaskListPageContext = ({ orderId }) => getContext({ orderId });
 
 const getExistingOrderTaskListPageContext = async ({ accessToken, orderId }) => {
-  const endpoint = getEndpoint({ endpointLocator: 'getExistingOrder', options: { orderId } });
+  const endpoint = getEndpoint({ endpointLocator: 'getOrderSummary', options: { orderId } });
   const order = await getData({ endpoint, accessToken, logger });
   logger.info('Existing order returned');
   return getContext({ orderId, orderDescription: order.description });
