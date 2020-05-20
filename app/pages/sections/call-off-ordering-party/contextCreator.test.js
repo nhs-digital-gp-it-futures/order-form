1db903a9d9227e3574d389d9c877691f24c664d3
kat1906 <katie.armstrong@bjss.com>
2020-05-20 09:01:08 +0100

    add contextCreator for  call-off-ordering-party

---
+++ app/pages/sections/call-off-ordering-party/contextCreator.test.js
@@ -0,0 +1,29 @@
+import manifest from './manifest.json';
+import { getContext } from './contextCreator';
+import { baseUrl } from '../../../config';
+
+describe('call-off-ordering-party contextCreator', () => {
+  describe('getContext', () => {
+    it('should return the contents of manifest', () => {
+      const context = getContext({});
+      expect(context.backLinkText).toEqual(manifest.backLinkText);
+      expect(context.description).toEqual(manifest.description);
+      expect(context.orgNameHeading).toEqual(manifest.orgNameHeading);
+      expect(context.odsCodeHeading).toEqual(manifest.odsCodeHeading);
+      expect(context.orgAddressHeading).toEqual(manifest.orgAddressHeading);
+      expect(context.saveButtonText).toEqual(manifest.saveButtonText);
+    });
+
+    it('should construct the backLinkHref', () => {
+      const orderId = 'order-id';
+      const context = getContext({ orderId });
+      expect(context.backlinkHref).toEqual(`${baseUrl}/organisation/${orderId}`);
+    });
+
+    it('should construct title woth orderId', () => {
+      const orderId = 'order-id';
+      const context = getContext({ orderId });
+      expect(context.title).toEqual('Call-off Ordering Party information for order-id');
+    });
+  });
+});
