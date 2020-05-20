ec8bb95ef49822fde654012b64fc9a9647331c48
kat1906 <katie.armstrong@bjss.com>
2020-05-20 15:25:37 +0100

    add data to context

--- app/pages/sections/call-off-ordering-party/contextCreator.test.js
+++ app/pages/sections/call-off-ordering-party/contextCreator.test.js
@@ -2,6 +2,22 @@ import manifest from './manifest.json';
 import { getContext } from './contextCreator';
 import { baseUrl } from '../../../config';
 
+const mockOrderingPartyData = {
+  name: 'Hampshire CC',
+  odsCode: 'AB3',
+  address: {
+    line1: 'line 1',
+    line2: 'line 2',
+    line3: 'line 3',
+    line4: null,
+    line5: 'line 5',
+    town: 'townville',
+    county: 'countyshire',
+    postcode: 'HA3 PSH',
+    country: 'UK',
+  },
+};
+
 describe('call-off-ordering-party contextCreator', () => {
   describe('getContext', () => {
     it('should return the contents of manifest', () => {
@@ -12,6 +28,13 @@ describe('call-off-ordering-party contextCreator', () => {
       expect(context.odsCodeHeading).toEqual(manifest.odsCodeHeading);
       expect(context.orgAddressHeading).toEqual(manifest.orgAddressHeading);
       expect(context.saveButtonText).toEqual(manifest.saveButtonText);
+    });
+
+    it('should add data', () => {
+      const context = getContext({ data: mockOrderingPartyData });
+      expect(context.name).toEqual(mockOrderingPartyData.name);
+      expect(context.odsCode).toEqual(mockOrderingPartyData.odsCode);
+      expect(context.address).toEqual(mockOrderingPartyData.address);
     });
 
     it('should construct the backLinkHref', () => {
