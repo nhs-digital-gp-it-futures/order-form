6514febe22addb1cf2e78727dd5f0be9380f045d
Fawad Ali <alibombaye@gmail.com>
2020-05-20 09:03:53 +0100

    add route for supplier that redirects to /search

--- app/routes.js
+++ app/routes.js
@@ -76,6 +76,15 @@ export const routes = (authProvider) => {
     res.status(200).send('call-off-ordering-party-page');
   }));
 
+  router.get('/organisation/:orderId/supplier', authProvider.authorise({ claim: 'ordering' }), withCatch(authProvider, async (req, res) => {
+    const { orderId } = req.params;
+    res.redirect(`${config.baseUrl}/organisation/${orderId}/supplier/search`);
+  }));
+
+  router.get('/organisation/:orderId/supplier/search', authProvider.authorise({ claim: 'ordering' }), withCatch(authProvider, async (req, res) => {
+    res.status(200).send('supplier search page');
+  }));
+
   router.get('*', (req) => {
     throw new ErrorContext({
       status: 404,
