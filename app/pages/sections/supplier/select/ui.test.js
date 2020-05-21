04528cb2e1ca1acdfa3e7e863381c2c57226a73e
Fawad Ali <alibombaye@gmail.com>
2020-05-21 21:10:20 +0100

    ui test for base supplier select page

--- app/pages/sections/supplier/select/ui.test.js
+++ app/pages/sections/supplier/select/ui.test.js
@@ -2,6 +2,7 @@ import nock from 'nock';
 import { ClientFunction, Selector } from 'testcafe';
 import { extractInnerText } from 'buying-catalogue-library';
 import { solutionsApiUrl } from '../../../../config';
+import content from './manifest.json';
 
 const pageUrl = 'http://localhost:1234/organisation/order-1/supplier/search/select';
 
@@ -43,6 +44,80 @@ test('when user is not authenticated - should navigate to the identity server lo
 
   await t
     .expect(getLocation()).eql('http://identity-server/login');
+});
+
+test('should render Supplier select page', async (t) => {
+  nock(solutionsApiUrl)
+    .get('/api/v1/suppliers?name=some-supp')
+    .reply(200, [{}]);
+
+  await pageSetup(t, true);
+  await t.navigateTo(`${pageUrl}?supplierNameToFind=some-supp`);
+  const page = Selector('[data-test-id="supplier-select-page"]');
+
+  await t
+    .expect(page.exists).ok();
+});
+
+test('should navigate to /organisation/order-1/supplier/search when click on backlink', async (t) => {
+  nock(solutionsApiUrl)
+    .get('/api/v1/suppliers?name=some-supp')
+    .reply(200, [{}]);
+
+  await pageSetup(t, true);
+  await t.navigateTo(`${pageUrl}?supplierNameToFind=some-supp`);
+
+  const goBackLink = Selector('[data-test-id="go-back-link"] a');
+
+  await t
+    .expect(goBackLink.exists).ok()
+    .click(goBackLink)
+    .expect(getLocation()).eql('http://localhost:1234/order/organisation/order-1/supplier/search');
+});
+
+test('should render the title', async (t) => {
+  nock(solutionsApiUrl)
+    .get('/api/v1/suppliers?name=some-supp')
+    .reply(200, [{}]);
+
+  await pageSetup(t, true);
+  await t.navigateTo(`${pageUrl}?supplierNameToFind=some-supp`);
+
+  const title = Selector('h1[data-test-id="supplier-select-page-title"]');
+
+  await t
+    .expect(title.exists).ok()
+    .expect(await extractInnerText(title)).eql(content.title);
+});
+
+test('should render the description', async (t) => {
+  nock(solutionsApiUrl)
+    .get('/api/v1/suppliers?name=some-supp')
+    .reply(200, [{}]);
+
+  await pageSetup(t, true);
+  await t.navigateTo(`${pageUrl}?supplierNameToFind=some-supp`);
+
+  const description = Selector('h2[data-test-id="supplier-select-page-description"]');
+
+  await t
+    .expect(description.exists).ok()
+    .expect(await extractInnerText(description)).eql(content.description);
+});
+
+test('should render the Continue button', async (t) => {
+  nock(solutionsApiUrl)
+    .get('/api/v1/suppliers?name=some-supp')
+    .reply(200, [{}]);
+
+  await pageSetup(t, true);
+  await t.navigateTo(`${pageUrl}?supplierNameToFind=some-supp`);
+
+  const button = Selector('[data-test-id="continue-button"] button');
+
+  await t
+    .expect(button.exists).ok()
+    .expect(await extractInnerText(button)).eql(content.continueButtonText);
 });
 
 test('should redirect to the search page if a supplier to find is not pass in the url', async (t) => {
