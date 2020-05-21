14c0f0b1158091478ad040a63be82322a5993aa0
kat1906 <katie.armstrong@bjss.com>
2020-05-21 14:22:47 +0100

    add putOrderingParty endpoint

--- app/endpoints.js
+++ app/endpoints.js
@@ -2,13 +2,17 @@ import { oidcBaseUri, orderApiUrl, organisationApiUrl } from './config';
 
 const endpoints = {
   getIdentityApiHealth: () => `${oidcBaseUri}/health/ready`,
+  // GET endpoints
   getOrders: options => `${orderApiUrl}/api/v1/organisations/${options.orgId}/orders`,
   getOrderSummary: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/summary`,
-  postDescription: () => `${orderApiUrl}/api/v1/orders`,
-  putDescription: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/description`,
   getDescription: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/description`,
   getCallOffOrderingParty: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/ordering-party`,
   getOrganisationById: options => `${organisationApiUrl}/api/v1/Organisations/${options.orgId}`,
+  // PUT endpoints
+  putDescription: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/description`,
+  putOrderingParty: options => `${orderApiUrl}/api/v1/orders/${options.orderId}/sections/ordering-party`,
+  // POST endpoints
+  postDescription: () => `${orderApiUrl}/api/v1/orders`,
 };
 
 export const getEndpoint = ({ endpointLocator, options }) => endpoints[endpointLocator](options);
