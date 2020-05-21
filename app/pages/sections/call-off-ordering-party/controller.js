4da445040db99638da8971ff6e13b179220ad88b
kat1906 <katie.armstrong@bjss.com>
2020-05-21 15:02:00 +0100

    added logic to call putData for call-off-ordering-party

--- app/pages/sections/call-off-ordering-party/controller.js
+++ app/pages/sections/call-off-ordering-party/controller.js
@@ -1,7 +1,31 @@
-import { getData } from 'buying-catalogue-library';
+import { getData, putData } from 'buying-catalogue-library';
 import { getContext } from './contextCreator';
 import { getEndpoint } from '../../../endpoints';
 import { logger } from '../../../logger';
+
+const formatPutData = data => ({
+  organisation: {
+    name: data.name,
+    odsCode: data.odsCode,
+    address: {
+      line1: data.line1,
+      line2: data.line2,
+      line3: data.line3,
+      line4: data.line4,
+      line5: data.line5,
+      town: data.town,
+      county: data.county,
+      postcode: data.postcode,
+      country: data.country,
+    },
+  },
+  primaryContact: {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    emailAddress: data.emailAddress,
+    telephoneNumber: data.telephoneNumber,
+  },
+});
 
 export const getCallOffOrderingPartyContext = async ({ orderId, orgId, accessToken }) => {
   try {
@@ -26,4 +50,26 @@ export const getCallOffOrderingPartyContext = async ({ orderId, orgId, accessTok
     }
   }
   throw new Error();
+};
+
+export const putCallOffOrderingParty = async ({
+  orgId, orderId, data, accessToken,
+}) => {
+  const endpoint = getEndpoint({ endpointLocator: 'putOrderingParty', options: { orderId } });
+  const body = formatPutData(data);
+  try {
+    await putData({
+      endpoint,
+      body,
+      organisationId: orgId,
+      accessToken,
+      logger,
+    });
+
+    logger.info(`Call off ordering party updated - order id: ${orderId}, ${JSON.stringify(data)}`);
+    return { success: true };
+  } catch (err) {
+    logger.error('Error updating call-off-ordering-party for order');
+    throw new Error();
+  }
 };
