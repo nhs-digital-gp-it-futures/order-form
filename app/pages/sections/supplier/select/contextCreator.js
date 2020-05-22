98cb29e417f901e1209f82ec50a433ac6c9fd122
Fawad Ali <alibombaye@gmail.com>
2020-05-22 10:15:08 +0100

    update context creator to return selected suppliers question

--- app/pages/sections/supplier/select/contextCreator.js
+++ app/pages/sections/supplier/select/contextCreator.js
@@ -1,9 +1,28 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../../config';
 
-export const getContext = ({ orderId }) => {
+const generateSupplierOptions = ({ suppliers }) => {
+  const supplierOptions = suppliers.map(supplier => ({
+    value: supplier.supplierId,
+    text: supplier.name,
+  }));
+
+  return supplierOptions;
+};
+
+const generateQuestionsContext = ({ suppliers }) => {
+  const questions = manifest.questions.map(question => ({
+    ...question,
+    options: generateSupplierOptions({ suppliers }),
+  }));
+
+  return questions;
+};
+
+export const getContext = ({ orderId, suppliers }) => {
   const context = ({
     ...manifest,
+    questions: suppliers && generateQuestionsContext({ suppliers }),
     backLinkHref: `${baseUrl}/organisation/${orderId}/supplier/search`,
   });
   return context;
--- app/pages/sections/supplier/select/contextCreator.test.js
+++ app/pages/sections/supplier/select/contextCreator.test.js
@@ -28,6 +28,42 @@ describe('supplier select contextCreator', () => {
       expect(context.description).toEqual(manifest.description);
     });
 
+    it('should return the select supplier question', () => {
+      const expectedContext = {
+        questions: [
+          {
+            id: 'selectSupplier',
+            mainAdvice: 'Select Supplier',
+            options: [
+              {
+                value: 'supplier-1',
+                text: 'Supplier 1',
+              },
+              {
+                value: 'supplier-2',
+                text: 'Supplier 2',
+              },
+            ],
+          },
+        ],
+      };
+
+      const suppliers = [
+        {
+          supplierId: 'supplier-1',
+          name: 'Supplier 1',
+        },
+        {
+          supplierId: 'supplier-2',
+          name: 'Supplier 2',
+        },
+      ];
+
+      const context = getContext({ suppliers });
+      expect(context.questions).toEqual(expectedContext.questions);
+    });
+
+
     it('should return the continueButtonText', () => {
       const context = getContext({});
       expect(context.continueButtonText).toEqual(manifest.continueButtonText);
