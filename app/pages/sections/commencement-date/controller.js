30fed2779448eaa74297bdfccd677d1fc6522431
kat1906 <katie.armstrong@bjss.com>
2020-05-26 17:01:26 +0100

    add put call to save data

--- app/pages/sections/commencement-date/controller.js
+++ app/pages/sections/commencement-date/controller.js
@@ -1,10 +1,29 @@
+import { putData } from 'buying-catalogue-library';
 import { getContext } from './contextCreator';
 import { getDateErrors } from './getDateErrors';
+import { getEndpoint } from '../../../endpoints';
+import { logger } from '../../../logger';
+
+const formatPutData = data => ({
+  commencementDate: `${data['commencementDate-year']}-${data['commencementDate-month']}-${data['commencementDate-day']}`,
+});
 
 export const getCommencementDateContext = async parmas => getContext(parmas);
 
-export const putCommencementDate = async ({ data }) => {
+export const putCommencementDate = async ({
+  orderId, data, accessToken,
+}) => {
   const errors = [getDateErrors(data)];
   if (errors[0]) return { success: false, errors };
+
+  const endpoint = getEndpoint({ endpointLocator: 'putCommencementDate', options: { orderId } });
+  const body = formatPutData(data);
+  await putData({
+    endpoint,
+    body,
+    accessToken,
+    logger,
+  });
+  logger.info(`Commencement date updated - order id: ${orderId}, ${JSON.stringify(data)}`);
   return { success: true };
 };
--- app/pages/sections/commencement-date/controller.test.js
+++ app/pages/sections/commencement-date/controller.test.js
@@ -1,11 +1,13 @@
+import { putData } from 'buying-catalogue-library';
 import { putCommencementDate } from './controller';
-
+import { logger } from '../../../logger';
+import { orderApiUrl } from '../../../config';
 import * as dateValidator from './getDateErrors';
 
+jest.mock('buying-catalogue-library');
 jest.mock('./getDateErrors', () => ({
   getDateErrors: jest.fn(),
 }));
-
 
 const mockData = {
   'commencementDate-day': '1',
@@ -26,27 +28,61 @@ describe('commencement-date controller', () => {
     });
 
     it('should call getDateErrors with the correct params', () => {
-      putCommencementDate({ data: mockData });
+      putCommencementDate({
+        orderId: 'order-id', data: mockData, accessToken: 'access_token',
+      });
 
       expect(dateValidator.getDateErrors.mock.calls.length).toEqual(1);
       expect(dateValidator.getDateErrors).toHaveBeenCalledWith(mockData);
     });
 
-    it('should return success as false and errors if found', async () => {
-      dateValidator.getDateErrors
-        .mockReturnValue(mockDataError);
-      const response = await putCommencementDate({ data: mockData });
-      expect(response.success).toEqual(false);
-      expect(response.errors.length).toEqual(1);
-      expect(response.errors[0]).toEqual(mockDataError);
+    describe('with errors', () => {
+      it('should return success as false if validation errors found', async () => {
+        dateValidator.getDateErrors
+          .mockReturnValueOnce(mockDataError);
+        const response = await putCommencementDate({
+          orderId: 'order-id', data: mockData, accessToken: 'access_token',
+        });
+        expect(response.success).toEqual(false);
+        expect(response.errors.length).toEqual(1);
+        expect(response.errors[0]).toEqual(mockDataError);
+      });
     });
 
-    it('should return success as true if errors not found', async () => {
-      dateValidator.getDateErrors
-        .mockReturnValue(null);
-      const response = await putCommencementDate({ data: mockData });
-      expect(response.success).toEqual(true);
-      expect(response.errors).toEqual(undefined);
+    describe('with no errors', () => {
+      beforeEach(() => {
+        putData.mockReset();
+      });
+
+      it('should call putData once with the correct params', async () => {
+        dateValidator.getDateErrors
+          .mockReturnValueOnce(null);
+        putData
+          .mockResolvedValueOnce({});
+
+        await putCommencementDate({
+          orderId: 'order-id', data: mockData, accessToken: 'access_token',
+        });
+        expect(putData.mock.calls.length).toEqual(1);
+        expect(putData).toHaveBeenCalledWith({
+          endpoint: `${orderApiUrl}/api/v1/orders/order-id/sections/commencement-date`,
+          body: { commencementDate: '2020-12-1' },
+          accessToken: 'access_token',
+          logger,
+        });
+      });
+
+      it('should return success as true if data is saved successfully', async () => {
+        dateValidator.getDateErrors
+          .mockReturnValueOnce(null);
+        putData
+          .mockResolvedValueOnce({});
+        const response = await putCommencementDate({
+          orderId: 'order-id', data: mockData, accessToken: 'access_token',
+        });
+        expect(response.success).toEqual(true);
+        expect(response.errors).toEqual(undefined);
+      });
     });
   });
 });
