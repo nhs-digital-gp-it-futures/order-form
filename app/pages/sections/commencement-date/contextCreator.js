b5c2ce4a0b1d2509a9815a82c3a281dd57bf826e
kat1906 <katie.armstrong@bjss.com>
2020-05-27 17:13:20 +0100

    add getErrorContext

--- app/pages/sections/commencement-date/contextCreator.js
+++ app/pages/sections/commencement-date/contextCreator.js
@@ -20,3 +20,25 @@ export const getContext = ({ orderId, data }) => ({
   title: `${manifest.title} ${orderId}`,
   backLinkHref: `${baseUrl}/organisation/${orderId}`,
 });
+
+const addErrorsToManifest = ({ validationErrors, modifiedManifest }) => {
+  const errorMessage = manifest.errorMessages[validationErrors[0].id];
+  const questionError = { message: errorMessage, fields: validationErrors[0].part || ['day', 'month', 'year'] };
+  const manifestWithErrors = { ...modifiedManifest };
+  manifestWithErrors.questions[0].error = questionError;
+  manifestWithErrors.errors = [{ text: errorMessage, href: '#commencementDate' }];
+
+  return manifestWithErrors;
+};
+
+export const getErrorContext = ({ validationErrors, orderId, data }) => {
+  const formattedDate = `${data['commencementDate-year']}-${data['commencementDate-month']}-${data['commencementDate-day']}`;
+  const modifiedManifest = addDataToManifest(formattedDate);
+  const manifestWithErrors = addErrorsToManifest({ validationErrors, modifiedManifest });
+
+  return {
+    ...manifestWithErrors,
+    title: `${manifest.title} ${orderId}`,
+    backLinkHref: `${baseUrl}/organisation/${orderId}`,
+  };
+};
