5a06130ecea1445e34dae5fddaa1acb5faab33ed
Fawad Ali <alibombaye@gmail.com>
2020-05-28 11:04:46 +0100

    function to populate data

--- app/pages/sections/supplier/supplier/contextCreator.js
+++ app/pages/sections/supplier/supplier/contextCreator.js
@@ -1,9 +1,23 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../../config';
 
-export const getContext = ({ orderId, supplierData }) => ({
-  ...manifest,
-  title: `${manifest.title} ${orderId}`,
-  supplierData,
-  backLinkHref: `${baseUrl}/organisation/${orderId}/supplier/search/select`,
-});
+const populateQuestionsWithData = ({ primaryContact }) => (
+  manifest.questions.map((question) => {
+    const modifiedQuestion = { ...question };
+    if (primaryContact[question.id]) {
+      modifiedQuestion.data = primaryContact[question.id];
+    }
+  })
+);
+
+export const getContext = ({ orderId, supplierData }) => {
+  return {
+    ...manifest,
+    questions: supplierData && supplierData.primaryContact
+      ? populateQuestionsWithData({ supplierData })
+      : manifest.questions,
+    title: `${manifest.title} ${orderId}`,
+    supplierData,
+    backLinkHref: `${baseUrl}/organisation/${orderId}/supplier/search/select`,
+  };
+};
