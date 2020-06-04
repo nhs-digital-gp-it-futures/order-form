dd4939b88d93abc770a50b2e5bcae8ace0c4e9c0
Fawad Ali <alibombaye@gmail.com>
2020-06-04 16:31:58 +0100

    update search query string

--- app/pages/sections/supplier/search/ui.test.js
+++ app/pages/sections/supplier/search/ui.test.js
@@ -121,7 +121,7 @@ test('should render the Search button', async (t) => {
 
 test('should redirect to /organisation/order-id/supplier/search/select when suppliers are returned', async (t) => {
   nock(solutionsApiUrl)
-    .get('/api/v1/suppliers?name=some-supp&limitToPublishedSolutions=true')
+    .get('/api/v1/suppliers?name=some-supp&solutionPublicationStatus=Published')
     .reply(200, [{}]);
 
   await pageSetup(t, true);
@@ -138,7 +138,7 @@ test('should redirect to /organisation/order-id/supplier/search/select when supp
 
 test('should render the error page if no suppliers are found', async (t) => {
   nock(solutionsApiUrl)
-    .get('/api/v1/suppliers?name=some-supp&limitToPublishedSolutions=true')
+    .get('/api/v1/suppliers?name=some-supp&solutionPublicationStatus=Published')
     .reply(200, []);
 
   await pageSetup(t, true);
