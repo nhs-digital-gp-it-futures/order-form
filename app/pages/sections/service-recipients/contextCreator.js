794076838cad408e3246cc3c00e41c3e481b12b0
kat1906 <katie.armstrong@bjss.com>
2020-06-04 14:42:34 +0100

    extract formatting tableData into function

--- app/pages/sections/service-recipients/contextCreator.js
+++ app/pages/sections/service-recipients/contextCreator.js
@@ -1,25 +1,24 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../config';
 
-export const getContext = ({
-  orderId, serviceRecipientsData = [], selectedServiceRecipientsData = [],
-}) => {
-  const tableData = serviceRecipientsData.map(data => ({
+const formatTableData = (serviceRecipientsData, selectedRecipientsData) => serviceRecipientsData
+  .map(data => ({
     organisationName: {
       id: `${data.odsCode}-id`,
       name: `${data.odsCode}-name`,
       value: data.odsCode,
       text: data.name,
-      checked: !!selectedServiceRecipientsData
+      checked: !!selectedRecipientsData
         .find(checkedRecipient => checkedRecipient.odsCode === data.odsCode),
     },
     odsCode: data.odsCode,
   }));
 
-  return {
-    ...manifest,
-    title: `${manifest.title} ${orderId}`,
-    backLinkHref: `${baseUrl}/organisation/${orderId}`,
-    tableData,
-  };
-};
+export const getContext = ({
+  orderId, serviceRecipientsData = [], selectedServiceRecipientsData = [],
+}) => ({
+  ...manifest,
+  title: `${manifest.title} ${orderId}`,
+  backLinkHref: `${baseUrl}/organisation/${orderId}`,
+  tableData: formatTableData(serviceRecipientsData, selectedServiceRecipientsData),
+});
