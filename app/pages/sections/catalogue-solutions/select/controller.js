99e7483fbbbcb46c17ea531b2c2451f9ffea226d
Fawad Ali <alibombaye@gmail.com>
2020-06-05 14:14:06 +0100

    add findSolutions function to controller

--- app/pages/sections/catalogue-solutions/select/controller.js
+++ app/pages/sections/catalogue-solutions/select/controller.js
@@ -1,3 +1,14 @@
+import { getData } from 'buying-catalogue-library';
+import { getEndpoint } from '../../../../endpoints';
+import { logger } from '../../../../logger';
 import { getContext } from './contextCreator';
 
 export const getSolutionsSelectPageContext = params => getContext(params);
+
+export const findSolutions = async ({ supplierId, accessToken }) => {
+  const endpoint = getEndpoint({ endpointLocator: 'getSolutionsForSupplier', options: { supplierId } });
+  const solutionsFound = await getData({ endpoint, accessToken, logger });
+  logger.info(`Searching for solutions for Supplier "${supplierId}" returned ${solutionsFound.length} solutions`);
+
+  return solutionsFound;
+};
--- app/pages/sections/catalogue-solutions/select/controller.test.js
+++ app/pages/sections/catalogue-solutions/select/controller.test.js
@@ -1,4 +1,7 @@
-import { getSolutionsSelectPageContext } from './controller';
+import { getData } from 'buying-catalogue-library';
+import { solutionsApiUrl } from '../../../../config';
+import { logger } from '../../../../logger';
+import { getSolutionsSelectPageContext, findSolutions } from './controller';
 import * as contextCreator from './contextCreator';
 
 jest.mock('buying-catalogue-library');
@@ -17,6 +20,25 @@ describe('catalogue-solutions select controller', () => {
 
       expect(contextCreator.getContext.mock.calls.length).toEqual(1);
       expect(contextCreator.getContext).toHaveBeenCalledWith({ orderId: 'order-1' });
+    });
+  });
+
+  describe('findSuppliers', () => {
+    afterEach(() => {
+      getData.mockReset();
+    });
+
+    it('should call getData once with the correct params', async () => {
+      getData
+        .mockResolvedValueOnce({ data: [] });
+
+      await findSolutions({ supplierId: 'supp-1', accessToken: 'access_token' });
+      expect(getData.mock.calls.length).toEqual(1);
+      expect(getData).toHaveBeenCalledWith({
+        endpoint: `${solutionsApiUrl}/api/v1/solutions?supplierId=supp-1`,
+        accessToken: 'access_token',
+        logger,
+      });
     });
   });
 });
