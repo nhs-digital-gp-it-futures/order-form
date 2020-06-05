6731aedbed65ff18819a6fc58fda74f8621848f0
Sam McCarthy <sam.mccarthy@bjss.com>
2020-06-05 11:03:56 +0100

    fixed failing test

--- app/pages/sections/service-recipients/ui-tests/withSavedData.ui.test.js
+++ app/pages/sections/service-recipients/ui-tests/withSavedData.ui.test.js
@@ -71,22 +71,22 @@ test('should render checked checkbox for each service recipient', async (t) => {
   const checkbox2Label = Selector('[data-test-id="organisation-name-checkbox-ods2"] label');
   await t
     .expect(checkbox1Input.exists).ok()
-    .expect(checkbox1Input.getAttribute('name')).eql('ods1-name')
-    .expect(checkbox1Input.getAttribute('id')).eql('ods1-id')
+    .expect(checkbox1Input.getAttribute('name')).eql('ods1')
+    .expect(checkbox1Input.getAttribute('id')).eql('ods1')
     .expect(checkbox1Input.getAttribute('type')).eql('checkbox')
     .expect(checkbox2Input.find(':checked')).ok()
     .expect(checkbox1Label.exists).ok()
     .expect(await extractInnerText(checkbox1Label)).eql(mockOapiData[0].name)
-    .expect(checkbox1Label.getAttribute('for')).eql('ods1-id')
+    .expect(checkbox1Label.getAttribute('for')).eql('ods1')
 
     .expect(checkbox2Input.exists).ok()
-    .expect(checkbox2Input.getAttribute('name')).eql('ods2-name')
-    .expect(checkbox2Input.getAttribute('id')).eql('ods2-id')
+    .expect(checkbox2Input.getAttribute('name')).eql('ods2')
+    .expect(checkbox2Input.getAttribute('id')).eql('ods2')
     .expect(checkbox2Input.getAttribute('type')).eql('checkbox')
     .expect(checkbox2Input.find(':checked')).ok()
     .expect(checkbox2Label.exists).ok()
     .expect(await extractInnerText(checkbox2Label)).eql(mockOapiData[1].name)
-    .expect(checkbox2Label.getAttribute('for')).eql('ods2-id');
+    .expect(checkbox2Label.getAttribute('for')).eql('ods2');
 });
 
 test('should render ods code for each service recipient', async (t) => {
