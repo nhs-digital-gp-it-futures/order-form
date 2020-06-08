9dfe2b9232c6487c2acb793cec4a02227c2851e1
Sam McCarthy <sam.mccarthy@bjss.com>
2020-06-08 10:30:42 +0100

    controller updated

--- app/pages/sections/catalogue-solutions/select-price/controller.js
+++ app/pages/sections/catalogue-solutions/select-price/controller.js
@@ -1,3 +1,21 @@
+import { getData } from 'buying-catalogue-library';
 import { getContext } from './contextCreator';
+import { getEndpoint } from '../../../../endpoints';
+import { logger } from '../../../../logger';
 
-export const getSolutionsPricePageContext = async ({ orderId }) => getContext({ orderId });
+export const getSolutionsPricePageContext = async ({
+  orderId, supplierId, accessToken,
+}) => {
+  const solutionsPricingEndpoint = getEndpoint({ endpointLocator: 'getSolutionsPricing', options: { supplierId } });
+  const solutionsPricingData = await getData({
+    endpoint: solutionsPricingEndpoint,
+    accessToken,
+    logger,
+  });
+  logger.info(`Solutions pricing for supplier with id: ${supplierId} found in OAPI.`);
+
+  return getContext({
+    orderId,
+    solutionsPricingData,
+  });
+};
---
+++ app/pages/sections/catalogue-solutions/select-price/controller.test.js
@@ -0,0 +1,67 @@
+import { getData } from 'buying-catalogue-library';
+import { solutionsApiUrl } from '../../../../config';
+import { logger } from '../../../../logger';
+import * as contextCreator from './contextCreator';
+import {
+  getSolutionsPricePageContext,
+} from './controller';
+
+jest.mock('buying-catalogue-library');
+jest.mock('../../../../logger');
+jest.mock('./contextCreator', () => ({
+  getContext: jest.fn(),
+}));
+
+const accessToken = 'access_token';
+const orderId = 'order-id';
+const supplierId = 'supp-1';
+
+const solutionsPricingData = {
+  id: supplierId,
+  name: 'name',
+  prices: [
+    {
+      type: 'flat',
+      currencyCode: 'GBP',
+      itemUnit: {
+        name: 'patient',
+        description: 'per patient',
+      },
+      timeUnit: {
+        name: 'year',
+        description: 'per year',
+      },
+      price: 1.64,
+    },
+  ],
+};
+
+describe('catalogue-solutions controller', () => {
+  describe('getCatalogueSolutionsPageContext', () => {
+    afterEach(() => {
+      getData.mockReset();
+      contextCreator.getContext.mockReset();
+    });
+
+    it('should call getData with the correct params', async () => {
+      getData.mockResolvedValueOnce({});
+
+      await getSolutionsPricePageContext({ orderId, supplierId, accessToken });
+      expect(getData.mock.calls.length).toEqual(1);
+      expect(getData).toHaveBeenCalledWith({
+        endpoint: `${solutionsApiUrl}/api/v1/solutions/${supplierId}/pricing`,
+        accessToken,
+        logger,
+      });
+    });
+
+    it('should call getContext with the correct params', async () => {
+      getData.mockResolvedValueOnce(solutionsPricingData);
+      contextCreator.getContext.mockResolvedValueOnce({});
+
+      await getSolutionsPricePageContext({ orderId, supplierId, accessToken });
+      expect(contextCreator.getContext.mock.calls.length).toEqual(1);
+      expect(contextCreator.getContext).toHaveBeenCalledWith({ orderId, solutionsPricingData });
+    });
+  });
+});
