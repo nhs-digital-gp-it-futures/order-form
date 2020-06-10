0b05807a76987b7fb3b47b858ac7a4ada4fa8109
Fawad Ali <alibombaye@gmail.com>
2020-06-10 09:16:02 +0100

    add getRecipients function to return recipients for the order

--- app/pages/sections/catalogue-solutions/select-recipient/controller.js
+++ app/pages/sections/catalogue-solutions/select-recipient/controller.js
@@ -1,3 +1,14 @@
+import { getData } from 'buying-catalogue-library';
+import { getEndpoint } from '../../../../endpoints';
+import { logger } from '../../../../logger';
 import { getContext } from './contextCreator';
 
 export const getSolutionRecipientPageContext = params => getContext(params);
+
+export const getRecipients = async ({ orderId, accessToken }) => {
+  const endpoint = getEndpoint({ endpointLocator: 'getSelectedServiceRecipientsFromOrdapi', options: { orderId } });
+  const serviceRecipientsData = await getData({ endpoint, accessToken, logger });
+  logger.info(`${serviceRecipientsData.length} service recipients returned for ${orderId}`);
+
+  return serviceRecipientsData.serviceRecipients;
+};
--- app/pages/sections/catalogue-solutions/select-recipient/controller.test.js
+++ app/pages/sections/catalogue-solutions/select-recipient/controller.test.js
@@ -1,5 +1,9 @@
+import { getData } from 'buying-catalogue-library';
+import { orderApiUrl } from '../../../../config';
+import { logger } from '../../../../logger';
 import {
   getSolutionRecipientPageContext,
+  getRecipients,
 } from './controller';
 import * as contextCreator from './contextCreator';
 
@@ -19,6 +23,25 @@ describe('catalogue-solutions select-solution controller', () => {
 
       expect(contextCreator.getContext.mock.calls.length).toEqual(1);
       expect(contextCreator.getContext).toHaveBeenCalledWith({ orderId: 'order-1', solutionName: 'Solution One' });
+    });
+  });
+
+  describe('getRecipients', () => {
+    afterEach(() => {
+      getData.mockReset();
+    });
+
+    it('should call getData once with the correct params', async () => {
+      getData
+        .mockResolvedValueOnce({ data: {} });
+
+      await getRecipients({ orderId: 'order-1', accessToken: 'access_token' });
+      expect(getData.mock.calls.length).toEqual(1);
+      expect(getData).toHaveBeenCalledWith({
+        endpoint: `${orderApiUrl}/api/v1/orders/order-1/sections/service-recipients`,
+        accessToken: 'access_token',
+        logger,
+      });
     });
   });
 });
