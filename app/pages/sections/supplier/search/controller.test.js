fbbbf89183c9d62ff4e8d35f1d0cfef38d21f9a8
kat1906 <katie.armstrong@bjss.com>
2020-06-11 10:11:38 +0100

    fix url encoding

--- app/pages/sections/supplier/search/controller.test.js
+++ app/pages/sections/supplier/search/controller.test.js
@@ -95,6 +95,19 @@ describe('supplier search controller', () => {
       });
     });
 
+    it('should encode the url', async () => {
+      getData
+        .mockResolvedValueOnce({ data: [] });
+
+      await findSuppliers({ name: '&', accessToken: 'access_token' });
+      expect(getData.mock.calls.length).toEqual(1);
+      expect(getData).toHaveBeenCalledWith({
+        endpoint: `${solutionsApiUrl}/api/v1/suppliers?name=%26&solutionPublicationStatus=Published`,
+        accessToken: 'access_token',
+        logger,
+      });
+    });
+
     it('should encode the endpoint called to find suppliers', async () => {
       getData
         .mockResolvedValueOnce({ data: [] });
