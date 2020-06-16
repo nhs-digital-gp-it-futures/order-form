df07d4cd35e11d527d374ea10487516de3bd149b
Sam McCarthy <sam.mccarthy@bjss.com>
2020-06-16 13:57:00 +0100

    updated tests

--- app/pages/sections/catalogue-solutions/order-item/controller.js
+++ app/pages/sections/catalogue-solutions/order-item/controller.js
@@ -1,18 +1,24 @@
+import { getData } from 'buying-catalogue-library';
 import { getContext } from './contextCreator';
-import { getSolution, getRecipients } from '../select/recipient/controller';
+import { logger } from '../../../../logger';
+import { getEndpoint } from '../../../../endpoints';
+import { getSolution } from '../select/recipient/controller';
+
+export const getRecipientName = async ({ selectedRecipientId, accessToken }) => {
+  const endpoint = getEndpoint({ api: 'oapi', endpointLocator: 'getServiceRecipient', options: { selectedRecipientId } });
+  const serviceRecipientData = await getData({ endpoint, accessToken, logger });
+  logger.info(`service recipient returned for ${selectedRecipientId}`);
+
+  return serviceRecipientData.name;
+};
 
 export const getOrderItemContext = async ({
-  orderId,
   selectedSolutionId,
   selectedRecipientId,
   accessToken,
 }) => {
   const solutionName = await getSolution({ solutionId: selectedSolutionId, accessToken }).name;
-  const serviceRecipients = await getRecipients({ orderId, accessToken });
-
-  const serviceRecipientName = serviceRecipients.find(
-    recipient => recipient.odsCode === selectedRecipientId,
-  ).name;
+  const serviceRecipientName = await getRecipientName({ selectedRecipientId, accessToken });
 
   return getContext({ solutionName, serviceRecipientName, odsCode: selectedRecipientId });
 };
