a04a5ea462d8f334476e7f2af0d10a25bc402ec2
Fawad Ali <alibombaye@gmail.com>
2020-06-19 09:29:58 +0100

    update context creator to return item for addedSolutionTable

--- app/pages/sections/catalogue-solutions/catalogue-solutions/contextCreator.js
+++ app/pages/sections/catalogue-solutions/catalogue-solutions/contextCreator.js
@@ -1,11 +1,35 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../../config';
 
-export const getContext = ({ orderId, orderDescription, catalogueSolutions }) => ({
+const generateItems = ({ orderId, catalogueSolutions }) => {
+  const items = catalogueSolutions.map((solution) => {
+    const columns = [];
+    columns.push(({
+      data: solution.solutionName,
+      href: `${baseUrl}/organisation/${orderId}/catalogue-solutions/${solution.orderItemId}`,
+      dataTestId: `${solution.orderItemId}-solutionName`,
+    }));
+    columns.push(({
+      data: `${solution.serviceRecipient.name} (${solution.serviceRecipient.odsCode})`,
+      dataTestId: `${solution.orderItemId}-serviceRecipient`,
+    }));
+    return columns;
+  });
+  return items;
+};
+
+const generateAddedSolutionTable = ({ orderId, addedSolutionTable, catalogueSolutions }) => ({
+  ...addedSolutionTable,
+  items: generateItems({ orderId, catalogueSolutions }),
+});
+
+export const getContext = ({ orderId, orderDescription, catalogueSolutions = [] }) => ({
   ...manifest,
   title: `${manifest.title} ${orderId}`,
   orderDescription,
-  catalogueSolutions,
+  addedSolutionTable: generateAddedSolutionTable({
+    orderId, addedSolutionTable: manifest.addedSolutionTable, catalogueSolutions,
+  }),
   addSolutionButtonHref: `${baseUrl}/organisation/${orderId}/catalogue-solutions/select/solution`,
   backLinkHref: `${baseUrl}/organisation/${orderId}`,
 });
--- app/pages/sections/catalogue-solutions/catalogue-solutions/contextCreator.test.js
+++ app/pages/sections/catalogue-solutions/catalogue-solutions/contextCreator.test.js
@@ -45,9 +45,68 @@ describe('catalogue-solutions contextCreator', () => {
       expect(context.noSolutionsText).toEqual(manifest.noSolutionsText);
     });
 
-    it('should return the catalogueSolutions provided', () => {
+    it('should return the addSolutionTable colummInfo and columnClass', () => {
       const context = getContext({ orderId: 'order-1', catalogueSolutions: [] });
-      expect(context.catalogueSolutions).toEqual([]);
+      expect(context.addedSolutionTable.columnInfo).toEqual(manifest.addedSolutionTable.columnInfo);
+      expect(context.addedSolutionTable.columnClass).toEqual(manifest.addedSolutionTable.columnClass);
+    });
+
+    it('should return the addSolutionTable without items if no catalogue solutions are provided', () => {
+      const context = getContext({ orderId: 'order-1', catalogueSolutions: [] });
+      expect(context.addedSolutionTable.items).toEqual([]);
+    });
+
+    it('should return the addSolutionTable with items when catalogue solutions are provided', () => {
+      const expectedContext = {
+        addedSolutionTable: {
+          ...manifest.addedSolutionTable,
+          items: [
+            [
+              {
+                data: 'Solution One',
+                href: '/order/organisation/order-1/catalogue-solutions/orderItem1',
+                dataTestId: 'orderItem1-solutionName',
+              },
+              {
+                data: 'Recipient One (recipient-1)',
+                dataTestId: 'orderItem1-serviceRecipient',
+              },
+            ],
+            [
+              {
+                data: 'Solution One',
+                href: '/order/organisation/order-1/catalogue-solutions/orderItem2',
+                dataTestId: 'orderItem2-solutionName',
+              },
+              {
+                data: 'Recipient Two (recipient-2)',
+                dataTestId: 'orderItem2-serviceRecipient',
+              },
+            ],
+          ],
+        },
+      };
+
+      const mockCatalogueSolutions = [
+        {
+          orderItemId: 'orderItem1',
+          solutionName: 'Solution One',
+          serviceRecipient: {
+            name: 'Recipient One',
+            odsCode: 'recipient-1',
+          },
+        },
+        {
+          orderItemId: 'orderItem2',
+          solutionName: 'Solution One',
+          serviceRecipient: {
+            name: 'Recipient Two',
+            odsCode: 'recipient-2',
+          },
+        },
+      ];
+      const context = getContext({ orderId: 'order-1', catalogueSolutions: mockCatalogueSolutions });
+      expect(context.addedSolutionTable).toEqual(expectedContext.addedSolutionTable);
     });
 
     it('should return the addSolutionButtonText', () => {
