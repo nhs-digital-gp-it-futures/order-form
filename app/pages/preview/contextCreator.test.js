907dc11827ea65ae315bd156b3d7d34d38c84e72
Fawad Ali <alibombaye@gmail.com>
2020-06-19 12:51:08 +0100

    added context creator for basic preview page

---
+++ app/pages/preview/contextCreator.test.js
@@ -0,0 +1,38 @@
+import manifest from './manifest.json';
+import { getContext } from './contextCreator';
+import { baseUrl } from '../../config';
+
+describe('order summary preview contextCreator', () => {
+  describe('getContext', () => {
+    it('should return the backLinkText', () => {
+      const context = getContext({ orderId: 'order-1' });
+      expect(context.backLinkText).toEqual(manifest.backLinkText);
+    });
+
+    it('should construct the backLinkHref', () => {
+      const orderId = 'order-id';
+      const context = getContext({ orderId });
+      expect(context.backLinkHref).toEqual(`${baseUrl}/organisation/${orderId}`);
+    });
+
+    it('should return the title', () => {
+      const context = getContext({ orderId: 'order-1' });
+      expect(context.title).toEqual(`${manifest.title} order-1`);
+    });
+
+    it('should return the description', () => {
+      const context = getContext({ orderId: 'order-1' });
+      expect(context.description).toEqual(manifest.description);
+    });
+
+    it('should return the orderDescriptionHeading', () => {
+      const context = getContext({ orderId: 'order-1' });
+      expect(context.orderDescriptionHeading).toEqual(manifest.orderDescriptionHeading);
+    });
+
+    it('should return the orderDescription provided', () => {
+      const context = getContext({ orderId: 'order-1', orderDescription: 'Some order description' });
+      expect(context.orderDescription).toEqual('Some order description');
+    });
+  });
+});
