d75b34303bbea759ac27e736bd55ab1c099aca3d
Fawad Ali <alibombaye@gmail.com>
2020-07-02 22:09:46 +0100

    remove console logs

--- app/pages/sections/getSectionErrorContext.js
+++ app/pages/sections/getSectionErrorContext.js
@@ -4,9 +4,6 @@ import { baseUrl } from '../../config';
 export const getSectionErrorContext = ({
   orderId, validationErrors, data, manifest,
 }) => {
-  console.log('manifest', manifest)
-  console.log('validationErrors', validationErrors)
-
   const formattedErrors = formatErrors({ manifest, errors: validationErrors });
   const modifiedManifest = addErrorsAndDataToManifest({ manifest, errors: formattedErrors, data });
   const allErrors = formatAllErrors(modifiedManifest.questions);
