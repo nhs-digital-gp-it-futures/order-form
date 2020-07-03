614d26a048ef0828cd79ddeec50b327335d5646b
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-07-03 11:13:45 +0100

    Store progress
    AB#5124
    AB#8149

--- app/pages/sections/additional-services/additional-services/contextCreator.js
+++ app/pages/sections/additional-services/additional-services/contextCreator.js
@@ -1,7 +1,10 @@
 import manifest from './manifest.json';
+import { baseUrl } from '../../../../config';
 
 export const getContext = ({ orderId, orderDescription }) => ({
   ...manifest,
   title: `${manifest.title} ${orderId}`,
   orderDescription,
+  addOrderItemButtonHref: `${baseUrl}/organisation/${orderId}/additional-services/select/solution`,
+  backLinkHref: `${baseUrl}/organisation/${orderId}`,
 });
---
+++ app/pages/sections/additional-services/additional-services/ui-tests/general.ui.test.js
@@ -0,0 +1,69 @@
+import nock from 'nock';
+import { ClientFunction, Selector } from 'testcafe';
+import content from '../manifest.json';
+
+const pageUrl = 'http://localhost:1234/order/organisation/order-1/additional-services';
+
+const setCookies = ClientFunction(() => {
+  const cookieValue = JSON.stringify({
+    id: '88421113', name: 'Cool Dude', ordering: 'manage', primaryOrganisationId: 'org-id',
+  });
+
+  document.cookie = `fakeToken=${cookieValue}`;
+});
+
+const mocks = () => {
+};
+
+const pageSetup = async (withAuth = true) => {
+  if (withAuth) {
+    mocks();
+    await setCookies();
+  }
+};
+
+const getLocation = ClientFunction(() => document.location.href);
+
+fixture('Additional-services page - general')
+  .page('http://localhost:1234/order/some-fake-page')
+  .afterEach(async (t) => {
+    const isDone = nock.isDone();
+    if (!isDone) {
+      nock.cleanAll();
+    }
+
+    await t.expect(isDone).ok('Not all nock interceptors were used!');
+  });
+
+test('when user is not authenticated - should navigate to the identity server login page', async (t) => {
+  nock('http://identity-server')
+    .get('/login')
+    .reply(200);
+
+  await pageSetup(false);
+  await t.navigateTo(pageUrl);
+
+  await t
+    .expect(getLocation()).eql('http://identity-server/login');
+});
+
+test('should render additional-services page', async (t) => {
+  await pageSetup();
+  await t.navigateTo(pageUrl);
+  const page = Selector('[data-test-id="additional-services-page"]');
+
+  await t
+    .expect(page.exists).ok();
+});
+
+test.only('should navigate to /organisation/order-1 when click on backLink', async (t) => {
+  await pageSetup();
+  await t.navigateTo(pageUrl);
+
+  const goBackLink = Selector('[data-test-id="go-back-link"] a');
+
+  await t
+    .expect(goBackLink.exists).ok()
+    .click(goBackLink)
+    .expect(getLocation()).eql('http://localhost:1234/order/organisation/order-1');
+});
