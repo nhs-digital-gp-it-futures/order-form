87bc3604e0c4d77ab534b97ef2dac1aff32844eb
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-07-05 11:55:03 +0100

    Add additional services page call put endpoint
    AB#5124
    AB#8152

--- app/pages/sections/additional-services/additional-services/ui-tests/general.ui.test.js
+++ app/pages/sections/additional-services/additional-services/ui-tests/general.ui.test.js
@@ -154,6 +154,10 @@ test('should render the Continue button', async (t) => {
 });
 
 test('should redirect to /organisation/order-1 when clicking the Continue button', async (t) => {
+  nock(orderApiUrl)
+    .put('/api/v1/orders/order-1/sections/additional-services')
+    .reply(200);
+
   await pageSetup();
   await t.navigateTo(pageUrl);
 
@@ -161,5 +165,5 @@ test('should redirect to /organisation/order-1 when clicking the Continue button
 
   await t
     .click(continueButton)
-    .expect(getLocation()).eql('http://localhost:1234/order/organisation/order-1/additional-services');
+    .expect(getLocation()).eql('http://localhost:1234/order/organisation/order-1');
 });
