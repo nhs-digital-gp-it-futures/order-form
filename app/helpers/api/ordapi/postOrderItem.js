ae18d7d3cb2d240caed3dedfccfd228fc7b2c52e
alibombaye <alibombaye@gmail.com>
2020-07-12 14:24:04 +0100

    made save of orderItem more generic and moved api post and put functions to the api helper folder

---
+++ app/helpers/api/ordapi/postOrderItem.js
@@ -0,0 +1,52 @@
+import { postData } from 'buying-catalogue-library';
+import { logger } from '../../../logger';
+import { getEndpoint } from '../../../endpoints';
+import { extractDate } from '../../controllers/extractDate';
+
+const formatPostData = ({
+  serviceRecipientId,
+  serviceRecipientName,
+  itemId,
+  itemName,
+  selectedPrice,
+  formData,
+}) => ({
+  ...selectedPrice,
+  serviceRecipient: {
+    name: serviceRecipientName,
+    odsCode: serviceRecipientId,
+  },
+  catalogueItemId: itemId,
+  catalogueItemName: itemName,
+  deliveryDate: extractDate('deliveryDate', formData),
+  quantity: parseInt(formData.quantity, 10),
+  estimationPeriod: formData.selectEstimationPeriod,
+  price: parseFloat(formData.price),
+});
+
+export const postOrderItem = async ({
+  accessToken,
+  orderId,
+  serviceRecipientId,
+  serviceRecipientName,
+  itemId,
+  itemName,
+  selectedPrice,
+  formData,
+}) => {
+  const endpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'postOrderItem', options: { orderId } });
+  const body = formatPostData({
+    serviceRecipientId,
+    serviceRecipientName,
+    itemId,
+    itemName,
+    selectedPrice,
+    formData,
+  });
+
+  await postData({
+    endpoint, body, accessToken, logger,
+  });
+  logger.info(`Order item for ${itemName} and ${serviceRecipientName} successfully created for order id: ${orderId}`);
+  return { success: true };
+};
--- app/pages/sections/additional-services/order-item/controller.js
+++ app/pages/sections/additional-services/order-item/controller.js
@@ -1,18 +1,11 @@
-import { postData, putData } from 'buying-catalogue-library';
 import { getContext, getErrorContext } from './contextCreator';
 import { logger } from '../../../../logger';
-import { getEndpoint } from '../../../../endpoints';
 import commonManifest from './commonManifest.json';
 import { getSelectedPriceManifest } from '../../../../helpers/controllers/manifestProvider';
-import { extractDate } from '../../../../helpers/controllers/extractDate';
+import { postOrderItem } from '../../../../helpers/api/ordapi/postOrderItem';
+import { putOrderItem } from '../../../../helpers/api/ordapi/putOrderItem';
 
 const formatFormData = ({ formData }) => ({
-  'deliveryDate-day': formData['deliveryDate-day']
-    ? formData['deliveryDate-day'].trim() : undefined,
-  'deliveryDate-month': formData['deliveryDate-month']
-    ? formData['deliveryDate-month'].trim() : undefined,
-  'deliveryDate-year': formData['deliveryDate-year']
-    ? formData['deliveryDate-year'].trim() : undefined,
   quantity: formData.quantity
     ? formData.quantity.trim() : undefined,
   price: formData.price && formData.price.length > 0
@@ -71,105 +64,30 @@ export const getOrderItemErrorPageContext = (params) => {
   return getErrorContext(updatedParams);
 };
 
-const formatPostData = ({
-  selectedRecipientId,
-  serviceRecipientName,
-  selectedSolutionId,
-  solutionName,
-  selectedPrice,
-  formData,
-}) => ({
-  ...selectedPrice,
-  serviceRecipient: {
-    name: serviceRecipientName,
-    odsCode: selectedRecipientId,
-  },
-  catalogueSolutionId: selectedSolutionId,
-  catalogueSolutionName: solutionName,
-  deliveryDate: extractDate('deliveryDate', formData),
-  quantity: parseInt(formData.quantity, 10),
-  estimationPeriod: formData.selectEstimationPeriod,
-  price: parseFloat(formData.price),
-});
-
-const postSolutionOrderItem = async ({
-  accessToken,
-  orderId,
-  selectedRecipientId,
-  serviceRecipientName,
-  selectedSolutionId,
-  solutionName,
-  selectedPrice,
-  formData,
-}) => {
-  const endpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'postSolutionOrderItem', options: { orderId } });
-  const body = formatPostData({
-    selectedRecipientId,
-    serviceRecipientName,
-    selectedSolutionId,
-    solutionName,
-    selectedPrice,
-    formData,
-  });
-
-  await postData({
-    endpoint, body, accessToken, logger,
-  });
-  logger.info(`Order item for ${solutionName} and ${serviceRecipientName} successfully created for order id: ${orderId}`);
-  return { success: true };
-};
-
-const formatPutData = ({
-  formData,
-}) => ({
-  deliveryDate: extractDate('deliveryDate', formData),
-  quantity: parseInt(formData.quantity, 10),
-  estimationPeriod: formData.selectEstimationPeriod,
-  price: parseFloat(formData.price),
-});
-
-const putSolutionOrderItem = async ({
-  accessToken,
-  orderId,
-  orderItemId,
-  formData,
-}) => {
-  const endpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'putCatalogueOrderItem', options: { orderId, orderItemId } });
-  const body = formatPutData({
-    formData,
-  });
-
-  await putData({
-    endpoint, body, accessToken, logger,
-  });
-  logger.info(`Order item successfully updated for order id: ${orderId} and order item id: ${orderItemId}`);
-  return { success: true };
-};
-
-export const saveSolutionOrderItem = async ({
+export const saveOrderItem = async ({
   orderId,
   orderItemId,
   accessToken,
-  selectedRecipientId,
+  serviceRecipientId,
   serviceRecipientName,
-  selectedSolutionId,
-  solutionName,
+  itemId,
+  itemName,
   selectedPrice,
   formData,
 }) => {
   try {
     const response = orderItemId === 'neworderitem'
-      ? await postSolutionOrderItem({
+      ? await postOrderItem({
         accessToken,
         orderId,
-        selectedRecipientId,
+        serviceRecipientId,
         serviceRecipientName,
-        selectedSolutionId,
-        solutionName,
+        itemId,
+        itemName,
         selectedPrice,
         formData,
       })
-      : await putSolutionOrderItem({
+      : await putOrderItem({
         accessToken,
         orderId,
         orderItemId,
@@ -181,7 +99,7 @@ export const saveSolutionOrderItem = async ({
       logger.info(`Follow validation errors returned from the API ${JSON.stringify(err.response.data.errors)}`);
       return err.response.data;
     }
-    logger.error(`Error saving order item for ${solutionName} and ${serviceRecipientName} for order id: ${orderId}`);
+    logger.error(`Error saving order item for ${itemName} and ${serviceRecipientName} for order id: ${orderId}`);
     throw new Error();
   }
 };
