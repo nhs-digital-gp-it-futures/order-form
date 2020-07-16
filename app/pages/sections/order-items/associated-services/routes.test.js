d20dff1b14e2d3d19da3fd3e632966332cc70742
bjss-chris <christopher.martin@bjss.com>
2020-07-16 13:58:25 +0100

    Add select associated service route
    AB#8418

---
+++ app/pages/sections/order-items/associated-services/routes.test.js
@@ -0,0 +1,68 @@
+import request from 'supertest';
+import {
+  FakeAuthProvider,
+  testAuthorisedGetPathForUnauthenticatedUser,
+  testAuthorisedGetPathForUnauthorisedUser,
+  fakeSessionManager,
+} from 'buying-catalogue-library';
+import { App } from '../../../../app';
+import { routes } from '../../../../routes';
+
+jest.mock('../../../../logger');
+
+const mockLogoutMethod = jest.fn().mockResolvedValue({});
+
+const mockAuthorisedJwtPayload = JSON.stringify({
+  id: '88421113',
+  name: 'Cool Dude',
+  ordering: 'manage',
+  primaryOrganisationId: 'org-id',
+});
+const mockAuthorisedCookie = `fakeToken=${mockAuthorisedJwtPayload}`;
+
+const mockUnauthorisedJwtPayload = JSON.stringify({
+  id: '88421113', name: 'Cool Dude',
+});
+const mockUnauthorisedCookie = `fakeToken=${mockUnauthorisedJwtPayload}`;
+
+const setUpFakeApp = () => {
+  const authProvider = new FakeAuthProvider(mockLogoutMethod);
+  const app = new App(authProvider).createApp();
+  app.use('/', routes(authProvider, fakeSessionManager()));
+  return app;
+};
+
+describe('associated-services section routes', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('GET /organisation/:orderId/associated-services', () => {
+    const path = '/organisation/some-order-id/associated-services';
+
+    it('should redirect to the login page if the user is not logged in', () => (
+      testAuthorisedGetPathForUnauthenticatedUser({
+        app: request(setUpFakeApp()), getPath: path, expectedRedirectPath: 'http://identity-server/login',
+      })
+    ));
+
+    it('should show the error page indicating the user is not authorised if the user is logged in but not authorised', () => (
+      testAuthorisedGetPathForUnauthorisedUser({
+        app: request(setUpFakeApp()),
+        getPath: path,
+        getPathCookies: [mockUnauthorisedCookie],
+        expectedPageId: 'data-test-id="error-title"',
+        expectedPageMessage: 'You are not authorised to view this page',
+      })
+    ));
+
+    it('should return the text "Additional services page" if authorised', async () => {
+      const res = await request(setUpFakeApp())
+        .get(path)
+        .set('Cookie', [mockAuthorisedCookie])
+        .expect(200);
+
+      expect(res.text).toEqual('Additional services page');
+    });
+  });
+});
