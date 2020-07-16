432c2955bae6ca743727d895def0fa5ad7ae770a
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-07-16 12:02:43 +0100

    Add missing tests
    Story: AB#7263
    Task: AB#8320

--- app/helpers/api/ordapi/getOrderItems.js
+++ app/helpers/api/ordapi/getOrderItems.js
@@ -5,7 +5,7 @@ import { getEndpoint } from '../../../endpoints';
 export const getOrderItems = async ({ orderId, catalogueItemType, accessToken }) => {
   const endpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'getOrderItems', options: { orderId, catalogueItemType } });
   const orderItems = await getData({ endpoint, accessToken, logger });
-  logger.info(`Order items returned for orderId ${orderId}`);
+  logger.info(`Found order items for type '${catalogueItemType}' and orderId '${orderId}'`);
 
   return orderItems;
 };
--- app/helpers/api/ordapi/getOrderItems.test.js
+++ app/helpers/api/ordapi/getOrderItems.test.js
@@ -10,14 +10,27 @@ describe('getOrderItems', () => {
     jest.resetAllMocks();
   });
 
+  it('should call getData once with no catalogue item type', async () => {
+    getData
+      .mockResolvedValueOnce({ data: [] });
+
+    await getOrderItems({ orderId: 'order-1', catalogueItemType: undefined, accessToken: 'access_token' });
+    expect(getData.mock.calls.length).toEqual(1);
+    expect(getData).toHaveBeenCalledWith({
+      endpoint: `${orderApiUrl}/api/v1/orders/order-1/order-items`,
+      accessToken: 'access_token',
+      logger,
+    });
+  });
+
   it('should call getData once with the correct params', async () => {
     getData
       .mockResolvedValueOnce({ data: [] });
 
-    await getOrderItems({ orderId: 'order-1', accessToken: 'access_token' });
+    await getOrderItems({ orderId: 'order-1', catalogueItemType: 'Solution', accessToken: 'access_token' });
     expect(getData.mock.calls.length).toEqual(1);
     expect(getData).toHaveBeenCalledWith({
-      endpoint: `${orderApiUrl}/api/v1/orders/order-1/order-items`,
+      endpoint: `${orderApiUrl}/api/v1/orders/order-1/order-items?catalogueItemType=Solution`,
       accessToken: 'access_token',
       logger,
     });
