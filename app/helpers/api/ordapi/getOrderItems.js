8683fba17c42d0b1e96f6e2eedf81777f5fbcf41
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-07-16 13:01:26 +0100

    Resolve PR comments
    Story: AB#7263
    Task: AB#8320

--- app/helpers/api/ordapi/getOrderItems.js
+++ app/helpers/api/ordapi/getOrderItems.js
@@ -1,9 +1,14 @@
 import { getData } from 'buying-catalogue-library';
 import { logger } from '../../../logger';
-import { getEndpoint } from '../../../endpoints';
+import { orderApiUrl } from '../../../config';
+
+const getOrderItemsEndpoint = (orderId, catalogueItemType) => {
+  const queryString = (catalogueItemType !== undefined ? `?catalogueItemType=${catalogueItemType}` : '');
+  return `${orderApiUrl}/api/v1/orders/${orderId}/order-items${queryString}`;
+};
 
 export const getOrderItems = async ({ orderId, catalogueItemType, accessToken }) => {
-  const endpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'getOrderItems', options: { orderId, catalogueItemType } });
+  const endpoint = getOrderItemsEndpoint(orderId, catalogueItemType);
   const orderItems = await getData({ endpoint, accessToken, logger });
   logger.info(`Found order items for type '${catalogueItemType}' and orderId '${orderId}'`);
 
