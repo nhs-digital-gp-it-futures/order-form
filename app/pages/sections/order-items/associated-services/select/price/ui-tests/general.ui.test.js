57df1f892c8348e33189182a772fca167a5fa4fa
Sam McCarthy <sam.mccarthy@bjss.com>
2020-07-17 13:12:43 +0100

    unused import

--- app/pages/sections/order-items/associated-services/select/price/ui-tests/general.ui.test.js
+++ app/pages/sections/order-items/associated-services/select/price/ui-tests/general.ui.test.js
@@ -2,7 +2,7 @@ import nock from 'nock';
 import { ClientFunction, Selector } from 'testcafe';
 import { extractInnerText } from 'buying-catalogue-library';
 import content from '../manifest.json';
-import { orderApiUrl, solutionsApiUrl } from '../../../../../../../config';
+import { solutionsApiUrl } from '../../../../../../../config';
 
 const pageUrl = 'http://localhost:1234/order/organisation/order-id/associated-services/select/associated-service/price';
 
