fd1b28e41d66aaf64194db0f7e813026624b1510
Ruth Berry <Ruth.berry@bjss.com>
2020-07-17 08:57:01 +0100

    5115: Create associated service page, init

---
+++ app/pages/sections/order-items/associated-services/dashboard/controller.test.js
@@ -0,0 +1,48 @@
+import { getData, putData } from 'buying-catalogue-library';
+import { getEndpoint } from '../../../../../endpoints';
+import { logger } from '../../../../../logger';
+import { getContext } from './contextCreator';
+import { getOrderItems } from '../../../../../helpers/api/ordapi/getOrderItems';
+
+export const getAdditionalServicesPageContext = async ({
+  orderId,
+  catalogueItemType,
+  accessToken,
+}) => {
+  const additionalServiceOrderItemsData = await getOrderItems({
+    orderId,
+    catalogueItemType,
+    accessToken,
+  });
+
+  const getOrderDescriptionDataEndpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'getDescription', options: { orderId } });
+  const orderDescriptionData = await getData({
+    endpoint: getOrderDescriptionDataEndpoint, accessToken, logger,
+  });
+
+  return getContext({
+    orderId,
+    orderDescription: orderDescriptionData ? orderDescriptionData.description : '',
+    orderItems: additionalServiceOrderItemsData,
+  });
+};
+
+export const putAdditionalServices = async ({ orderId, accessToken }) => {
+  const putAdditionalServicesEndpoint = getEndpoint({ api: 'ordapi', endpointLocator: 'putAdditionalServices', options: { orderId } });
+  try {
+    const body = {
+      status: 'complete',
+    };
+
+    await putData({
+      endpoint: putAdditionalServicesEndpoint,
+      body,
+      accessToken,
+      logger,
+    });
+    return { success: true };
+  } catch (err) {
+    logger.error(`Error updating additional-services for ${orderId}`);
+    throw new Error();
+  }
+};
