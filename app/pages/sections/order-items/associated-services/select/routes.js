50bb166af5aca32d43782868bcfd20741f1dadc5
bjss-chris <christopher.martin@bjss.com>
2020-07-17 14:46:56 +0100

    Get associated services from BAPI
    AB#8421

--- app/pages/sections/order-items/associated-services/select/routes.js
+++ app/pages/sections/order-items/associated-services/select/routes.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { logger } from '../../../../../logger';
 import config from '../../../../../config';
 import { withCatch, extractAccessToken } from '../../../../../helpers/routes/routerHelper';
-import { getAssociatedServicePageContext } from './associated-service/controller';
+import { getAssociatedServicePageContext, findAssociatedServices } from './associated-service/controller';
 
 const router = express.Router({ mergeParams: true });
 
-export const associatedServicesSelectRoutes = (authProvider, addContext) => {
+export const associatedServicesSelectRoutes = (authProvider, addContext, sessionManager) => {
   router.get('/', authProvider.authorise({ claim: 'ordering' }), withCatch(logger, authProvider, async (req, res) => {
     const { orderId } = req.params;
     return res.redirect(`${config.baseUrl}/organisation/${orderId}/associated-services/select/associated-service`);
@@ -17,6 +17,10 @@ export const associatedServicesSelectRoutes = (authProvider, addContext) => {
     authProvider.authorise({ claim: 'ordering' }),
     withCatch(logger, authProvider, async (req, res) => {
       const { orderId } = req.params;
+      const associatedServices = await findAssociatedServices({ req, sessionManager });
+
+      // Temporary inclusion until associated services displayed on page
+      logger.info(`Found the following associated services: ${JSON.stringify(associatedServices)}`);
 
       const context = getAssociatedServicePageContext({
         orderId,
