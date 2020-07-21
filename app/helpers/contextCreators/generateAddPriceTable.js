3d79a8f09dbe5c9a97e1868cf4cfbc504c74c7e6
alibombaye <alibombaye@gmail.com>
2020-07-21 11:59:22 +0100

    add ui test to check for 0 price val;ue

--- app/helpers/contextCreators/generateAddPriceTable.js
+++ app/helpers/contextCreators/generateAddPriceTable.js
@@ -7,7 +7,7 @@ export const generateAddPriceTable = ({
     ...addPriceTable.cellInfo.price,
     question: {
       ...addPriceTable.cellInfo.price.question,
-      data: price && price.toString(),
+      data: price !== undefined ? `${price}` : undefined,
       error: errorMap && errorMap.price
         ? { message: errorMap.price.errorMessages.join(', ') }
         : undefined,
