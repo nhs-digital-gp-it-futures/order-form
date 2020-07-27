b086294aa38c4fdcb76b2a671fd02bdf08ec43f4
bjss-chris <christopher.martin@bjss.com>
2020-07-27 16:42:05 +0100

    Fix UI tests
    AB#8734

--- app/pages/task-list/existingorder/ui-tests/ui.test.js
+++ app/pages/task-list/existingorder/ui-tests/ui.test.js
@@ -746,7 +746,7 @@ test('should render task 8 item 1 as text if recipients saved and count 1', asyn
     .expect(task8Item1.find('a').exists).notOk();
 });
 
-test('should only render task 8 item 1 as a link if recipients saved, catalogue solution saved, additional service saved and associated service saved', async (t) => {
+test('should render task 8 item 1 as text if recipients saved, catalogue solution saved, additional service saved and associated service saved', async (t) => {
   const mockOrderSummary = generateMockOrderSummary([
     { id: 'ordering-party', status: 'complete' },
     { id: 'supplier', status: 'complete' },
@@ -762,9 +762,7 @@ test('should only render task 8 item 1 as a link if recipients saved, catalogue 
   const task8Item1 = Selector('li[data-test-id="task-7-item-0"]');
 
   await t
-    .expect(task8Item1.find('a').exists).ok()
-    .click(task8Item1.find('a'))
-    .expect(getLocation()).eql(`http://localhost:1234${baseUrl}/organisation/order-id/funding-sources`);
+    .expect(task8Item1.find('a').exists).notOk();
 });
 
 test('should only render task 8 item 1 as a link if recipients saved and count 0, associated service saved and count 1', async (t) => {
@@ -786,7 +784,7 @@ test('should only render task 8 item 1 as a link if recipients saved and count 0
     .expect(getLocation()).eql(`http://localhost:1234${baseUrl}/organisation/order-id/funding-sources`);
 });
 
-test('should only render task 8 item 1 as a link if recipients saved and count 1, Catalogue solution saved and count 1 and associated service saved and count 1', async (t) => {
+test('should only render task 8 item 1 as a link if recipients saved and count 1, Catalogue solution saved and count 0 and associated service saved and count 1', async (t) => {
   const mockOrderSummary = generateMockOrderSummary([
     { id: 'ordering-party', status: 'complete' },
     { id: 'supplier', status: 'complete' },
@@ -806,7 +804,7 @@ test('should only render task 8 item 1 as a link if recipients saved and count 1
     .expect(getLocation()).eql(`http://localhost:1234${baseUrl}/organisation/order-id/funding-sources`);
 });
 
-test('should only render task 8 item 1 as a link if recipients saved and count 1, Catalogue solution saved and count 1 and associated service saved and count 1', async (t) => {
+test('should render task 8 item 1 as text if recipients saved and count 0, Catalogue solution saved and count 0 and associated service saved and count 0', async (t) => {
   const mockOrderSummary = generateMockOrderSummary([
     { id: 'ordering-party', status: 'complete' },
     { id: 'supplier', status: 'complete' },
@@ -821,9 +819,7 @@ test('should only render task 8 item 1 as a link if recipients saved and count 1
   const task8Item1 = Selector('li[data-test-id="task-7-item-0"]');
 
   await t
-    .expect(task8Item1.find('a').exists).ok()
-    .click(task8Item1.find('a'))
-    .expect(getLocation()).eql(`http://localhost:1234${baseUrl}/organisation/order-id/funding-sources`);
+    .expect(task8Item1.find('a').exists).notOk();
 });
 
 test('should only render task 8 item 1 as a link if recipients saved and count 1, Catalogue solution saved and count 1 and associated service saved and count 1', async (t) => {
@@ -832,8 +828,8 @@ test('should only render task 8 item 1 as a link if recipients saved and count 1
     { id: 'supplier', status: 'complete' },
     { id: 'commencement-date', status: 'complete' },
     { id: 'service-recipients', status: 'complete', count: 1 },
-    { id: 'catalogue-solutions', status: 'complete', count: 0 },
-    { id: 'associated-services', status: 'complete', count: 0 },
+    { id: 'catalogue-solutions', status: 'complete', count: 1 },
+    { id: 'associated-services', status: 'complete', count: 1 },
   ]);
   await pageSetup({ ...defaultPageSetup, mockData: mockOrderSummary });
   await t.navigateTo(pageUrl);
