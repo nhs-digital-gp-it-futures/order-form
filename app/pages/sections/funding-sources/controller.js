ff0985c6d6d3bba92222fcd5349c08757a2d57f8
Sam McCarthy <sam.mccarthy@bjss.com>
2020-07-27 14:51:10 +0100

    fixed bug

--- app/pages/sections/funding-sources/controller.js
+++ app/pages/sections/funding-sources/controller.js
@@ -6,7 +6,8 @@ export const getFundingSourcesContext = async ({ orderId, fundingSource }) => ge
 export const getFundingSourcesErrorPageContext = params => getErrorContext(params);
 
 export const validateFundingSourcesForm = ({ data }) => {
-  if (data.selectFundingSource && typeof data.selectFundingSource === 'boolean') {
+  console.log(data);
+  if (data.selectFundingSource && data.selectFundingSource.trim().length > 0) {
     return { success: true };
   }
 
