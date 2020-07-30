365b698dacee3c606bdca7cf797244f9f1c8a1b2
Sam McCarthy <sam.mccarthy@bjss.com>
2020-07-30 09:41:10 +0100

    wip

--- app/pages/sections/service-recipients/contextCreator.js
+++ app/pages/sections/service-recipients/contextCreator.js
@@ -8,19 +8,47 @@ const getCheckedStatus = ({ selectStatus, data, selectedRecipientIdsData = [] })
     .find(checkedRecipient => checkedRecipient.odsCode === data.odsCode);
 };
 
-const formatTableData = ({
-  selectStatus, serviceRecipientsData, selectedRecipientIdsData,
-}) => serviceRecipientsData
-  .map(data => ({
-    organisationName: {
-      id: data.odsCode,
-      name: data.odsCode,
-      value: data.name,
-      text: data.name,
-      checked: getCheckedStatus({ selectStatus, data, selectedRecipientIdsData }),
-    },
-    odsCode: data.odsCode,
-  }));
+const generateItems = ({
+  selectStatus,
+  serviceRecipientsData,
+  selectedRecipientIdsData,
+}) => {
+  const items = serviceRecipientsData.map((serviceRecipient) => {
+    const columns = [];
+    columns.push(({
+      question: {
+        dataTestId: `${serviceRecipient.name}-organisationName`,
+        checked: getCheckedStatus({ selectStatus, serviceRecipient, selectedRecipientIdsData }),
+        type: 'checkbox',
+        id: `${serviceRecipient.name}-organisationName`,
+        name: serviceRecipient.odsCode,
+        value: serviceRecipient.name,
+        text: serviceRecipient.name,
+      },
+      classes: 'nhsuk-u-font-size-12',
+    }));
+    columns.push(({
+      data: serviceRecipient.odsCode,
+      dataTestId: `${serviceRecipient.odsCode}-odsCode`,
+    }));
+    return columns;
+  });
+  return items;
+};
+
+const generateAddedOrderItemsTable = ({
+  selectStatus,
+  serviceRecipientsTable,
+  serviceRecipientsData,
+  selectedRecipientIdsData,
+}) => ({
+  ...serviceRecipientsTable,
+  items: generateItems({
+    selectStatus,
+    serviceRecipientsData,
+    selectedRecipientIdsData,
+  }),
+});
 
 export const getContext = ({
   orderId, serviceRecipientsData = [], selectedRecipientIdsData = [], selectStatus,
@@ -30,8 +58,11 @@ export const getContext = ({
     ...manifest,
     title: `${manifest.title} ${orderId}`,
     backLinkHref: `${baseUrl}/organisation/${orderId}`,
-    tableData: formatTableData({
-      selectStatus, serviceRecipientsData, selectedRecipientIdsData,
+    serviceRecipientsTable: generateAddedOrderItemsTable({
+      selectStatus,
+      serviceRecipientsTable: manifest.serviceRecipientsTable,
+      serviceRecipientsData,
+      selectedRecipientIdsData,
     }),
     selectDeselectButtonAction: `${baseUrl}/organisation/${orderId}/service-recipients`,
     selectStatus: toggledStatus,
