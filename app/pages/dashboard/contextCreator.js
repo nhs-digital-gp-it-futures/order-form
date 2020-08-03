58623d3d0716a1f608f1af44becd9637a8bf731a
bjss-chris <christopher.martin@bjss.com>
2020-08-03 15:25:26 +0100

    Use bcTableFlexible component instead of bcTable
    AB#8849

--- app/pages/dashboard/contextCreator.js
+++ app/pages/dashboard/contextCreator.js
@@ -3,28 +3,35 @@ import { baseUrl } from '../../config';
 import { formatDate } from '../../helpers/common/dateFormatter';
 
 export const getContext = ({ orgName, ordersData = [] }) => {
+  const { cellInfo } = manifest;
+
   const formattedOrders = ordersData.reduce((acc, order) => {
     const formattedOrder = [
       {
         data: order.orderId,
         href: `${baseUrl}/organisation/${order.orderId}`,
         dataTestId: `${order.orderId}-id`,
+        classes: cellInfo.orderId.classes,
       },
       {
         data: order.description,
         dataTestId: `${order.orderId}-description`,
+        classes: cellInfo.description.classes,
       },
       {
         data: order.lastUpdatedBy,
         dataTestId: `${order.orderId}-lastUpdatedBy`,
+        classes: cellInfo.lastUpdatedBy.classes,
       },
       {
         data: formatDate(order.lastUpdated),
         dataTestId: `${order.orderId}-lastUpdated`,
+        classes: cellInfo.lastUpdated.classes,
       },
       {
         data: formatDate(order.dateCreated),
         dataTestId: `${order.orderId}-dateCreated`,
+        classes: cellInfo.dateCreated.classes,
       },
     ];
     if (order.status.toLowerCase() === 'complete') acc.completeOrders.push(formattedOrder);
