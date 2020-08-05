6122e7973ad2db777e21409352cfee8f5bad403e
alibombaye <alibombaye@gmail.com>
2020-08-05 11:12:58 +0100

    lint fix

--- app/pages/sections/routes.js
+++ app/pages/sections/routes.js
@@ -20,7 +20,6 @@ import { additionalServicesRoutes } from './order-items/additional-services/rout
 import { associatedServicesRoutes } from './order-items/associated-services/routes';
 import { getFundingSource } from '../../helpers/api/ordapi/getFundingSource';
 import { putFundingSource } from '../../helpers/api/ordapi/putFundingSource';
-import { sessionKeys } from '../../helpers/routes/sessionHelper';
 
 const router = express.Router({ mergeParams: true });
 
