c8379153c313b91eeee3184a3661a15fd39068b3
alibombaye <alibombaye@gmail.com>
2020-08-06 18:26:59 +0100

    updated context creator to generate completed and incomplete order tables

--- app/pages/dashboard/contextCreator.js
+++ app/pages/dashboard/contextCreator.js
@@ -2,66 +2,104 @@ import manifest from './manifest.json';
 import { baseUrl } from '../../config';
 import { formatDate } from '../../helpers/common/dateFormatter';
 
-const formatOrderData = ({ ordersData = [] }) => ordersData.reduce((acc, order) => {
-  const isComplete = order.status.toLowerCase() === 'complete';
+const generateIncompletedOrdersTable = (incompletedOrders, incompleteOrdersTable) => {
+  const items = incompletedOrders.map((order) => {
+    const columns = [
+      {
+        data: order.orderId,
+        href: `${baseUrl}/organisation/${order.orderId}`,
+        dataTestId: `${order.orderId}-id`,
+        classes: incompleteOrdersTable.cellInfo.orderId.classes,
+      },
+      {
+        data: order.description,
+        dataTestId: `${order.orderId}-description`,
+        classes: incompleteOrdersTable.cellInfo.description.classes,
+      },
+      {
+        data: order.lastUpdatedBy,
+        dataTestId: `${order.orderId}-lastUpdatedBy`,
+        classes: incompleteOrdersTable.cellInfo.lastUpdatedBy.classes,
+      },
+      {
+        data: formatDate(order.lastUpdated),
+        dataTestId: `${order.orderId}-lastUpdated`,
+        classes: incompleteOrdersTable.cellInfo.lastUpdated.classes,
+      },
+      {
+        data: formatDate(order.dateCreated),
+        dataTestId: `${order.orderId}-dateCreated`,
+        classes: incompleteOrdersTable.cellInfo.dateCreated.classes,
+      },
+    ];
 
-  const cellInfo = isComplete
-    ? manifest.completeOrdersTable.cellInfo
-    : manifest.incompleteOrdersTable.cellInfo;
+    return columns;
+  });
 
-  const formattedOrder = [
-    {
-      data: order.orderId,
-      href: `${baseUrl}/organisation/${order.orderId}`,
-      dataTestId: `${order.orderId}-id`,
-      classes: cellInfo.orderId.classes,
-    },
-    {
-      data: order.description,
-      dataTestId: `${order.orderId}-description`,
-      classes: cellInfo.description.classes,
-    },
-    {
-      data: order.lastUpdatedBy,
-      dataTestId: `${order.orderId}-lastUpdatedBy`,
-      classes: cellInfo.lastUpdatedBy.classes,
-    },
-    {
-      data: isComplete ? formatDate(order.dateCompleted) : formatDate(order.lastUpdated),
-      dataTestId: isComplete ? `${order.orderId}-dateCompleted` : `${order.orderId}-lastUpdated`,
-      classes: isComplete ? cellInfo.dateCompleted.classes : cellInfo.lastUpdated.classes,
-    },
-    {
-      data: formatDate(order.dateCreated),
-      dataTestId: `${order.orderId}-dateCreated`,
-      classes: cellInfo.dateCreated.classes,
-    },
-  ];
+  return {
+    ...incompleteOrdersTable,
+    items,
+  };
+};
 
-  if (isComplete) {
-    formattedOrder.push({
-      data: order.automaticallyProcessed ? 'Yes' : 'No',
-      dataTestId: `${order.orderId}-automaticallyProcessed`,
-      classes: cellInfo.automaticallyProcessed.classes,
-    });
+const generateCompletedOrdersTable = (completedOrders, completeOrdersTable) => {
+  const items = completedOrders.map((order) => {
+    const columns = [
+      {
+        data: order.orderId,
+        href: `${baseUrl}/organisation/${order.orderId}`,
+        dataTestId: `${order.orderId}-id`,
+        classes: completeOrdersTable.cellInfo.orderId.classes,
+      },
+      {
+        data: order.description,
+        dataTestId: `${order.orderId}-description`,
+        classes: completeOrdersTable.cellInfo.description.classes,
+      },
+      {
+        data: order.lastUpdatedBy,
+        dataTestId: `${order.orderId}-lastUpdatedBy`,
+        classes: completeOrdersTable.cellInfo.lastUpdatedBy.classes,
+      },
+      {
+        data: formatDate(order.dateCompleted),
+        dataTestId: `${order.orderId}-dateCompleted`,
+        classes: completeOrdersTable.cellInfo.dateCompleted.classes,
+      },
+      {
+        data: formatDate(order.dateCreated),
+        dataTestId: `${order.orderId}-dateCreated`,
+        classes: completeOrdersTable.cellInfo.dateCreated.classes,
+      },
+      {
+        data: order.automaticallyProcessed ? 'Yes' : 'No',
+        dataTestId: `${order.orderId}-automaticallyProcessed`,
+        classes: completeOrdersTable.cellInfo.automaticallyProcessed.classes,
+      },
+    ];
 
-    acc.completeOrders.push(formattedOrder);
-  } else {
-    acc.incompleteOrders.push(formattedOrder);
-  }
+    return columns;
+  });
 
-  return acc;
-}, { completeOrders: [], incompleteOrders: [] });
+  return {
+    ...completeOrdersTable,
+    items,
+  };
+};
 
-export const getContext = ({ orgName, ordersData = [] }) => {
-  const formattedOrders = formatOrderData({ ordersData });
+export const getContext = ({ orgName, completedOrders = [], incompletedOrders = [] }) => {
+  // const formattedOrders = formatOrderData({ ordersData });
 
   return ({
     ...manifest,
     title: `${orgName} orders`,
     newOrderButtonHref: `${baseUrl}/organisation/neworder`,
     proxyLinkHref: '#',
-    completeOrders: formattedOrders.completeOrders,
-    incompleteOrders: formattedOrders.incompleteOrders,
+    completeOrders: generateCompletedOrdersTable(
+      completedOrders, manifest.completeOrdersTable,
+    ),
+    incompleteOrders: generateIncompletedOrdersTable(
+      incompletedOrders, manifest.incompleteOrdersTable,
+    ),
   });
 };
