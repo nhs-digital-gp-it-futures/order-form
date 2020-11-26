8e5d966e288ecbcaeaf8be702d766980a0ca6e79
Sam McCarthy <sam.mccarthy@bjss.com>
2020-11-26 14:59:35 +0000

    fixes

--- app/helpers/common/transformApiValidationResponse.test.js
+++ app/helpers/common/transformApiValidationResponse.test.js
@@ -6,7 +6,7 @@ describe('transformApiValidationResponse', () => {
   const quantity = 'Quantity';
   const quantityError = 'QuantityRequired';
 
-  it.only('should transform a non-bulk validation error response', () => {
+  it('should transform a non-bulk validation error response', () => {
     const errors = transformApiValidationResponse({ [`${deliveryDate}`]: [deliveryDateError] });
 
     expect(errors).toEqual([{ field: deliveryDate, id: deliveryDateError }]);
