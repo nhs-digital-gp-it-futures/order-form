aa5529f5764e9c1b578362d8b02fb081c7cf35c1
Neha Jain <neha.jain@mastek.com>
2021-03-18 08:17:11 +0000

    price validation bug fix

--- app/helpers/common/priceFormatter.js
+++ app/helpers/common/priceFormatter.js
@@ -9,10 +9,13 @@ export const formatNumber = ({
 
 export const formatPrice = ({ value, minimumFractionDigits = 2, maximumFractionDigits = 4 }) => {
   if (minimumFractionDigits === 0) {
-    if (Number.isInteger(Number.parseFloat(value))) {
-      return formatNumber({ value, maximumFractionDigits });
+    if (!Number.isNaN(Number(value)) && !Number.isNaN(parseFloat(value))) {
+      if (Number.isInteger(Number.parseFloat(value))) {
+        return formatNumber({ value, maximumFractionDigits });
+      }
+      return formatNumber({ value, minimumFractionDigits: 2, maximumFractionDigits });
     }
-    return formatNumber({ value, minimumFractionDigits: 2, maximumFractionDigits });
+    return value;
   }
   return formatNumber({ value, minimumFractionDigits, maximumFractionDigits });
 };
