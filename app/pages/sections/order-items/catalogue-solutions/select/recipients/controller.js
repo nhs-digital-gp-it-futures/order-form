a6f83efac0588c2dc1991ab34572fdf167a276c8
Nicholas Newlands <NickNewlands>
2021-05-04 10:19:25 +0100

    Fix select/deselect button
    Ticket: AB#14261
    Story: AB#7944

--- app/pages/sections/order-items/catalogue-solutions/select/recipients/controller.js
+++ app/pages/sections/order-items/catalogue-solutions/select/recipients/controller.js
@@ -3,6 +3,20 @@ import { getContext, getErrorContext } from './contextCreator';
 
 export const getSelectSolutionPriceEndpoint = (orderId, orderItemId) => `/organisation/${orderId}/catalogue-solutions/${orderItemId}`;
 
+export const getSelectStatus = ({ selectStatus, selectedRecipients, serviceRecipients }) => {
+  if (selectStatus) {
+    return selectStatus;
+  }
+
+  if (selectedRecipients
+    && serviceRecipients
+    && selectedRecipients.length === serviceRecipients.length) {
+    return 'select';
+  }
+
+  return false;
+};
+
 export const getServiceRecipientsContext = async ({
   orderId, itemName, selectStatus, serviceRecipients, selectedRecipients, solutionPrices,
   manifest, orderType,
